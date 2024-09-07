af6581dd45a67498166b1e92193c8f368b2c04cc
Adam Vlček <vlcek-adam@seznam.cz>
2024-09-07 19:27:30 +0200

    deleted laravel

--- .devcontainer/start.sh
+++ .devcontainer/start.sh
@@ -10,21 +10,17 @@ sudo chmod -R 770 $containerWorkspace
 ## npm service config
 apt-get update && apt-get install -y supervisor
 sudo cp /workspaces/buffet-rezervace/.devcontainer/npm_serv.conf /etc/supervisor/conf.d/
-sudo cp /workspaces/buffet-rezervace/.devcontainer/laravel.conf /etc/supervisor/conf.d/
 sudo supervisord -c /etc/supervisor/supervisord.conf
 sudo supervisorctl reread
 sudo supervisorctl update
 sudo supervisorctl start npm_serv
 
 ## apache config not in use
-<<noapache
 sudo su - vscode -c "cd /workspaces/buffet-rezervace/buffet-api && composer install"
 sudo a2enmod rewrite
 sudo a2enmod actions
-sudo a2enmod headers
 sudo service apache2 restart
 
 ## html folder clone
 sudo rm -r /var/www/html
 ln -s $containerWorkspace/buffet-api /var/www/html
-noapache
