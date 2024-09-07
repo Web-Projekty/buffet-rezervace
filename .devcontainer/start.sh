eedfe87452b71c56c876a74ff5b5f5417db2cc2c
Adam Vlček <vlcek-adam@seznam.cz>
2024-09-07 17:59:06 +0200

    started using laravel server instead of apache

--- .devcontainer/start.sh
+++ .devcontainer/start.sh
@@ -3,19 +3,21 @@ containerWorkspace=$(pwd)
 export WORKSPACE_DIR=$(pwd)
 
 ## changes rights
-#sudo chown -R www-data:www-data $containerWorkspace
-#sudo usermod -aG www-data vscode
-#sudo chmod -R 770 $containerWorkspace
+sudo chown -R vscode:vscode $containerWorkspace
+sudo usermod -aG vscode www-data
+sudo chmod -R 770 $containerWorkspace
 
 ## npm service config
 apt-get update && apt-get install -y supervisor
 sudo cp /workspaces/buffet-rezervace/.devcontainer/npm_serv.conf /etc/supervisor/conf.d/
+sudo cp /workspaces/buffet-rezervace/.devcontainer/laravel.conf /etc/supervisor/conf.d/
 sudo supervisord -c /etc/supervisor/supervisord.conf
 sudo supervisorctl reread
 sudo supervisorctl update
 sudo supervisorctl start npm_serv
 
-## apache config
+## apache config not in use
+<<noapache
 sudo su - vscode -c "cd /workspaces/buffet-rezervace/buffet-api && composer install"
 sudo a2enmod rewrite
 sudo a2enmod actions
@@ -25,3 +27,4 @@ sudo service apache2 restart
 ## html folder clone
 sudo rm -r /var/www/html
 ln -s $containerWorkspace/buffet-api /var/www/html
+noapache
