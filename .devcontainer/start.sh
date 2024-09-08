8620d21d4507461fe4565247f31c5f06b32262ce
Adam Vlček <vlcek-adam@seznam.cz>
2024-09-08 10:13:09 +0200

    mysqli extension for php

--- .devcontainer/start.sh
+++ .devcontainer/start.sh
@@ -16,7 +16,14 @@ sudo supervisorctl update
 sudo supervisorctl start npm_serv
 
 ## apache config not in use
+
+# configure php.ini
+sudo cp /usr/local/etc/php/php.ini-development /usr/local/etc/php/php.ini
+sudo sed -i 's/;extension=mysqli/extension=mysqli/' /usr/local/etc/php/php.ini
+sudo docker-php-ext-install mysqli # install mysqli extension
+# compose buffet-api
 sudo su - vscode -c "cd /workspaces/buffet-rezervace/buffet-api && composer install"
+# configure apache2 directly
 sudo a2enmod rewrite
 sudo a2enmod actions
 sudo service apache2 restart
