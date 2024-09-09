c29bf7ce64c01d5ac9e34773ecce96a6d6c23c04
Adam Vlček <vlcek-adam@seznam.cz>
2024-09-09 21:37:04 +0200

    started work on register function

--- buffet-api/src/Api/AuthApi.php
+++ buffet-api/src/Api/AuthApi.php
@@ -1,1 +1,20 @@
 <?php
+
+namespace Buffet\Api;
+
+use Buffet\Database\Database;
+
+class AuthApi
+{
+
+    function register($username, $password)
+    {
+        $db = new Database;
+
+        $password = password_hash($password, PASSWORD_BCRYPT);
+
+        if ($db->isDuplicate("users", "username", $username)) {
+            return ["duplicate"];
+        }
+    }
+}
