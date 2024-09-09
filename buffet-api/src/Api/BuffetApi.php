894792a530a26cf2b7bad8b330b9a944fdbfbae7
Adam Vlček <vlcek-adam@seznam.cz>
2024-09-09 21:34:57 +0200

    made basic api handling system

--- buffet-api/src/Api/BuffetApi.php
+++ buffet-api/src/Api/BuffetApi.php
@@ -2,7 +2,7 @@
 
 namespace Buffet\Api;
 
-use Buffet\Database\Database;
+use Buffet\Api\AuthApi;
 use Psr\Http\Message\ServerRequestInterface as RequestInterface;
 use Psr\Http\Message\ResponseInterface;
 
@@ -12,13 +12,63 @@ class BuffetApi
     {
         $output = $this->handleApiCall();
 
+        if ($output == null || !isset($output) || empty($output)) {
+            $output = ['success' => false, 'error' => "api returned a null value"];
+        }
+
         $html->getBody()->write(json_encode($output));
         return $html->withHeader('Content-type', 'application/json');
     }
 
     function handleApiCall()
     {
-        return $this->getPostJson();
+        $request = $this->getPostJson();
+
+        if (empty($request['requestType']) || !isset($request['requestType'])) {
+            return ['success' => false, 'error' => "undefined requestType"];
+        }
+
+        switch ($request['requestType']) {
+            case "test":
+                return $this->handleTest($request);
+                break;
+            case "register";
+                return $this->handleRegister($request);
+                break;
+            default:
+                return ['success' => false, 'error' => "tried calling non-existent api requestType"];
+                break;
+        }
+    }
+
+    function handleRegister($request)
+    {
+        $this->hasAllMembers($request, ["username", "password"]);
+
+        $username = $request['username'];
+        $password = $request['password'];
+
+        $auth = new AuthApi;
+
+        return $auth->register($username, $password);
+    }
+
+    function handleTest($request)
+    {
+        return $request;
+    }
+
+
+    function hasAllMembers($request, $members)
+    {
+        foreach ($members as $member) {
+            if (!isset($request[$member]) || empty($request[$member])) {
+                header('Content-Type: application/json');
+                echo json_encode(['success' => false, 'error' => "missing field: " . $member]);
+                die;
+            }
+        }
+        return true;
     }
 
     function getPostJson()
