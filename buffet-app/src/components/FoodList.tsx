6e7a22065d3e7069c3527b534e5cfca254178e6c
florixak <florixakcz@seznam.cz>
2024-09-10 18:41:19 +0200

    Update

--- buffet-app/src/components/FoodList.tsx
+++ buffet-app/src/components/FoodList.tsx
@@ -103,7 +103,7 @@ const dummyFood: DummyFoodType[] = [
 
 const FoodList = () => {
   return (
-    <div className="mt-[15rem] flex flex-col items-center justify-center">
+    <div className="mb-[3rem] mt-[15rem] flex flex-col items-center justify-center">
       <div className="grid grid-cols-1 gap-10 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
         {dummyFood.map((food) => {
           return (
--- buffet-app/src/components/Header.tsx
+++ buffet-app/src/components/Header.tsx
@@ -1,19 +1,23 @@
 import { Link } from "react-router-dom";
+import { LuShoppingCart } from "react-icons/lu";
 import Logo from "/logo.svg";
 import Navbar from "./Navbar";
 
 const Header = () => {
   return (
-    <header className="fixed z-50 flex h-40 w-full flex-row items-center justify-around bg-orange-200 text-xl text-black shadow-sm shadow-gray-500">
-      <Link>
+    <header className="fixed z-50 flex h-40 w-full flex-row items-center justify-evenly bg-orange-300 text-xl text-black shadow-sm shadow-gray-500">
+      <Link to={"/"}>
         <img
           src={Logo}
           width={140}
           alt="Hamburger Logo"
-          className="rounded-full bg-white p-2"
+          className="rounded-full bg-white p-1"
         />
       </Link>
       <Navbar />
+      <div className="rounded-full bg-white p-4 hover:cursor-pointer">
+        <LuShoppingCart size={48} />
+      </div>
     </header>
   );
 };
