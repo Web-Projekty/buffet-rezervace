b35d5ecd759b4c7aad472bfa0f0b36a2326df841
Adam Vlček <vlcek-adam@seznam.cz>
2024-09-10 23:43:01 +0200

    started work on JWT class

--- buffet-api/src/Api/JWTApi.php
+++ buffet-api/src/Api/JWTApi.php
@@ -1,1 +1,20 @@
 <?php
+namespace Buffet\Api;
+
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+
+class JWTApi{
+function getToken(){
+    $key = 'example_key';
+    $host = $_SERVER['HTTP_HOST'];
+            $payload = [
+                'iss' => 'https://buffet.vlastas.cc',
+                'iat' => rand(0,99999999999),
+                'nbf' => 1357000000
+            ];
+
+            $jwt = JWT::encode($payload, $key, 'HS384');
+            $dec = JWT::decode($jwt, new Key($key, 'HS384'));
+}
+}
