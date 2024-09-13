ea84f34e245f54ca10cb637f0abbed1771ae4cb1
Adam Vlček <79417338+Wlczak@users.noreply.github.com>
2024-09-13 08:21:36 +0000

    first test example

---
+++ buffet-api/tests/CredManagerTest.php
@@ -0,0 +1,13 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+final class CredManagerTest extends TestCase
+{
+    public function testTest(): void
+    {
+        $string = '';
+
+        $this->assertSame($string, "");
+    }
+}
