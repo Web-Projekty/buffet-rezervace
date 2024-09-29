350da97a801ccab93671018c5d5b75e3b3f21bda
Adam Vlček <vlcek-adam@seznam.cz>
2024-09-29 18:26:51 +0200

    created Success enum
    has the same structure as Error enum

---
+++ buffet-api/src/Types/Success.php
@@ -0,0 +1,34 @@
+<?php
+
+declare (strict_types = 1);
+
+namespace Buffet\Types;
+
+enum Success: string {
+    ############################ Api ############################
+    // api methods
+
+    ############################ Auth ############################
+    // registration
+    case Registration = 'Registered successfully';
+
+    /**
+     * @return string
+     */
+    public function getValue(): string
+    {
+        if ($this->isProd()) {
+            return "Api call finished successfully";
+        }
+        return $this->value;
+    }
+
+    /**
+     * @return bool
+     */
+    private function isProd(): bool
+    {
+        $isProd = false;
+        return $isProd;
+    }
+}
