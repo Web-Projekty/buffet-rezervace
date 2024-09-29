efe4e1311fe0a78ba97acfd6e023903eb2a70367
Adam Vlček <vlcek-adam@seznam.cz>
2024-09-29 17:58:22 +0200

    errors differ in dev from prod
    retruns specific errors when in dev and a general error when in prod

--- buffet-api/src/Types/Error.php
+++ buffet-api/src/Types/Error.php
@@ -1,17 +1,40 @@
 <?php
 
+declare (strict_types = 1);
+
 namespace Buffet\Types;
 
 enum Error: string {
+    ############################ Api ############################
+    // api methods
     case NonExistentMethod = 'Tried to call a non-existent api method';
     case StatusPending = 'Status is still pending';
     case InvalidDataType = 'Handling api call resulted in invalid data type';
+    // api keys
+    case MissingRequestKeys = 'Some or all of the request kays have been left undefined';
+    case MissingPayloadKeys = 'Some or all of the payload kays have been left undefined';
+
+    ############################ Auth ############################
+    // registration
+    case UserInUse = 'Username is in use';
 
     /**
-     * @return mixed
+     * @return string
      */
     public function getValue(): string
     {
+        if ($this->isProd()) {
+            return "Oops something has gone wrong";
+        }
         return $this->value;
+    }
+
+    /**
+     * @return bool
+     */
+    private function isProd(): bool
+    {
+        $isProd = false;
+        return $isProd;
     }
 }
