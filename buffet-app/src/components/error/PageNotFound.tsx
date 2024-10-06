d8b9dbebf3d334f1658d6e113430f4fabd7a1298
florixak <florixakcz@seznam.cz>
2024-10-06 20:31:58 +0200

    Add page not found

---
+++ buffet-app/src/components/error/PageNotFound.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PageNotFound = () => {
+  return (
+    <div className="mb-[3rem] mt-[10rem] w-full text-center text-2xl font-bold text-white">
+      Page Not Found
+    </div>
+  );
+};
+
+export default PageNotFound;
