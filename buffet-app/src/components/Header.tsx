dfeed25a89d394d4787a4cfcc9269ca95c9de6f6
ptakondrej <ptakondrej@spseplzen.cz>
2024-10-09 18:40:06 +0200

    Add header scroll shrink

--- buffet-app/src/components/Header.tsx
+++ buffet-app/src/components/Header.tsx
@@ -1,10 +1,31 @@
 import { Link } from "react-router-dom";
 import Logo from "../assets/images/logo.svg";
 import Navbar from "./Navbar";
+import { useEffect, useState } from "react";
 
 const Header = () => {
+  const [isShrunk, setIsShrunk] = useState<boolean>(false);
+
+  useEffect(() => {
+    const handleScroll = (): void => {
+      if (window.scrollY > 50) {
+        setIsShrunk(true);
+      } else {
+        setIsShrunk(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <header className="fixed z-50 flex h-28 w-full flex-row items-center justify-between bg-primary px-10 text-xl text-black shadow-lg shadow-black md:px-32">
+    <header
+      className={`fixed z-50 flex ${isShrunk ? "h-14" : "h-28"} w-full flex-row items-center justify-between bg-primary px-10 text-xl text-black shadow-lg shadow-black md:px-32`}
+    >
       <Link to={"/"}>
         <img
           src={Logo}
