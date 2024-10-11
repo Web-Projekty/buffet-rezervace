b9227b087985f969e1ef40ca42fefe19d828b0b8
ptakondrej <ptakondrej@spseplzen.cz>
2024-10-11 09:13:37 +0200

    Update Cart Modal styles

--- buffet-app/src/components/cart/CartModal.tsx
+++ buffet-app/src/components/cart/CartModal.tsx
@@ -30,6 +30,8 @@ const CartModal = () => {
     navigate("/cart");
   };
 
+  const isCartEmpty = cartItems.length === 0;
+
   return (
     <AnimatePresence>
       {isOpen && (
@@ -38,9 +40,9 @@ const CartModal = () => {
           animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
           transition={{ duration: 0.3 }}
-          className="fixed left-[50%] top-[50%] z-50 h-screen w-screen -translate-x-1/2 -translate-y-1/2 transform bg-transparentBlack"
+          className="fixed left-[50%] top-[50%] z-[60] h-screen w-screen -translate-x-1/2 -translate-y-1/2 transform bg-transparentBlack"
         >
-          <div className="fixed left-[50%] top-[50%] z-50 -translate-x-1/2 -translate-y-1/2 transform">
+          <div className="fixed left-[50%] top-[50%] -translate-x-1/2 -translate-y-1/2 transform">
             <motion.div
               {...scaleUpAnimation(0.3)}
               className="flex h-[500px] w-[800px] flex-col items-center justify-between rounded-lg bg-slate-800 shadow-md shadow-black"
@@ -49,30 +51,38 @@ const CartModal = () => {
                 <h1>Your Cart</h1>
               </div>
 
-              <div className="grid w-full grid-cols-3 gap-5 overflow-hidden px-5">
+              <div
+                className={`grid w-full ${isCartEmpty ? "grid-cols-1" : "grid-cols-2"} gap-5 overflow-auto p-5`}
+              >
                 {cartItems.length === 0 && (
-                  <p className="text-center text-white">Your cart is empty</p>
+                  <p className="text-center text-white">
+                    Váš košík je prázdný. 😢
+                  </p>
                 )}
                 {cartItems.map((item) => {
                   return (
                     <div
                       key={item.id}
-                      className="flex w-full flex-row items-center justify-between gap-2 rounded-lg bg-white p-4 text-black shadow-md"
+                      className="flex w-full flex-row items-center justify-between rounded-lg bg-white p-4 text-black shadow-md"
                     >
-                      <img
-                        src={item.image}
-                        alt={item.name}
-                        className="h-20 w-20 rounded-md object-cover"
-                      />
-                      <div className="flex w-full flex-col justify-center">
-                        <h1 className="text-xl font-bold">{item.name}</h1>
+                      <div className="flex w-full flex-row items-center gap-2">
+                        <img
+                          src={item.image}
+                          alt={item.name}
+                          className="h-20 w-20 rounded-md object-cover"
+                        />
+                        <div className="flex flex-col">
+                          <h1 className="text-xl font-bold">{item.name}</h1>
 
-                        <p className="font-bold text-gray-700">
-                          {formatCurrency(
-                            item.price * getItemQuantity(item.id),
-                          )}
-                        </p>
+                          <p className="font-bold text-gray-700">
+                            {formatCurrency(
+                              item.price * getItemQuantity(item.id),
+                            )}
+                          </p>
+                        </div>
+                      </div>
 
+                      <div className="flex flex-col justify-center">
                         <div className="flex w-full flex-row items-center gap-2 font-semibold text-black">
                           <button onClick={() => handleRemoveItem(item.id)}>
                             -
