dc88050f7d061fa147a6a628c6682ca24aa47bb3
ptakondrej <ptakondrej@spseplzen.cz>
2024-10-19 18:33:16 +0200

    Update modal animation

--- buffet-app/src/components/Modal.tsx
+++ buffet-app/src/components/Modal.tsx
@@ -6,15 +6,19 @@ type Modal = {
   darkBackground?: boolean;
 };
 
+const modalShowAnimation = {
+  initial: { opacity: 0 },
+  animate: { opacity: 1 },
+  exit: { opacity: 0 },
+  transition: { duration: 0.3 },
+};
+
 const Modal = ({ children, isOpen, darkBackground }: Modal) => {
   return (
     <AnimatePresence>
       {isOpen && (
         <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-          transition={{ duration: 0.3 }}
+          {...modalShowAnimation}
           className={`fixed left-[50%] top-[50%] z-[60] h-screen w-screen -translate-x-1/2 -translate-y-1/2 transform ${darkBackground ? "bg-transparentBlack" : "bg-transparent"}`}
         >
           <div className="fixed left-[50%] top-[50%] -translate-x-1/2 -translate-y-1/2 transform">
