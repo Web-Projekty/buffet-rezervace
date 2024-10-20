33023a8962bbad44716f67cfb9665437d7e66204
ptakondrej <ptakondrej@spseplzen.cz>
2024-10-20 22:04:35 +0200

    Update disable button style

--- buffet-app/src/components/Button.tsx
+++ buffet-app/src/components/Button.tsx
@@ -15,7 +15,7 @@ const Button = ({
 }: Button) => {
   return (
     <button
-      className={`rounded-md border border-cyan-900 bg-cyan-500 p-2 text-white hover:bg-cyan-600 ${additionalStyles} ${disabled ? "bg-cyan-600" : ""}`}
+      className={`rounded-md border border-cyan-900 bg-cyan-500 p-2 text-white hover:bg-cyan-600 ${additionalStyles} ${disabled ? "cursor-not-allowed bg-cyan-600" : ""}`}
       type={type}
       onClick={onClick}
       disabled={disabled}
