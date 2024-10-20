79e1a087a7d3c1710315485526b569cd892b7d26
ptakondrej <ptakondrej@spseplzen.cz>
2024-10-20 22:03:57 +0200

    Change font (prototype)

--- buffet-app/src/App.tsx
+++ buffet-app/src/App.tsx
@@ -6,7 +6,7 @@ import CartModal from "./components/cart/CartModal";
 
 const App = () => {
   return (
-    <div className="relative flex min-h-screen flex-col overflow-x-hidden bg-slate-800 font-Lato">
+    <div className="relative flex min-h-screen flex-col overflow-x-hidden bg-slate-800 font-sans">
       <Header />
       <RouteScrollToTop />
       <CartModal />
