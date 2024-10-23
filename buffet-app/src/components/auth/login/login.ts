c6f0ba5f497e5cb4ef44c5bb6ed7dc3a98a7d547
ptakondrej <ptakondrej@spseplzen.cz>
2024-10-23 21:20:18 +0200

    Fix time expire comparison

--- buffet-app/src/components/auth/login/login.ts
+++ buffet-app/src/components/auth/login/login.ts
@@ -4,21 +4,22 @@ import { jwtDecode, JwtPayload } from "jwt-decode";
 export const setTokenExpiration = (token: string): void => {
   //const expirationTime = new Date().getTime() + expiresIn * 1000;
   const decoded: JwtPayload = jwtDecode(token);
-  const expiration: number | undefined = decoded.exp;
-  console.log("Expiration time", expiration);
-
-  // localStorage.setItem("tokenExpiration", expirationTime.toString());
-  Cookies.set("tokenExpiration", expiration!.toString(), {
-    expires: expiration! / 86400,
+  const expirationTime: number | undefined = decoded.exp;
+  console.log("Expiration time", expirationTime);
+  if (!expirationTime) return;
+  Cookies.set("tokenExpiration", expirationTime.toString(), {
+    expires: expirationTime / 86400,
   });
 };
 
 export const isTokenExpired = (): boolean => {
   // const expirationTime = localStorage.getItem("tokenExpiration");
   const expirationTime = Cookies.get("tokenExpiration");
+  console.log("Expiration time", expirationTime);
+  console.log("Current time", new Date().getTime());
   if (!expirationTime) return true;
 
-  return new Date().getTime() > parseInt(expirationTime, 10);
+  return new Date().getTime() > parseInt(expirationTime, 10) * 1000;
 };
 
 export const removeTokenExpiration = (): void => {
