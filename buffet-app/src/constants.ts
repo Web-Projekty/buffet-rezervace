7240013da2c32f54b9439514604ad8ea37336bbe
ptakondrej <ptakondrej@spseplzen.cz>
2024-11-03 22:35:57 +0100

    Update constants

--- buffet-app/src/constants.ts
+++ buffet-app/src/constants.ts
@@ -4,6 +4,6 @@ export const itemsPerPage: number = 9;
 
 export const adminOrdersPerPage: number = 10;
 
-export const ordersPerPage: number = 3;
+export const ordersPerPage: number = 2;
 
 export const cartLocalStorageKey: string = "cartItems";
