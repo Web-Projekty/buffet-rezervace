e45e9268872cf70f42a74bf3d1c91d1470dbc29c
Adam Vlček <vlcek-adam@seznam.cz>
2024-11-06 12:42:37 +0100

    formatted

--- buffet-api/src/Api/AuthApi.php
+++ buffet-api/src/Api/AuthApi.php
@@ -4,7 +4,6 @@ declare (strict_types = 1);
 
 namespace Buffet\Api;
 
-use Buffet\Database\Database;
 use Buffet\Database\Models\UserModel;
 use Buffet\Types\ApiResponse;
 use Buffet\Types\Error;
@@ -19,7 +18,7 @@ class AuthApi
      * Returns errors when user profile cannot be created
      *
      * @param  ApiResponse
-     * @return ApiResponse    Api response
+     * @return ApiResponse   Api response
      */
 
     function register(ApiResponse $response): ApiResponse
