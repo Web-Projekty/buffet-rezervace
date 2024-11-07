ce3a94e615a5f51a4a82f6911fcf989475cb069d
Adam Vlček <vlcek-adam@seznam.cz>
2024-11-07 20:18:03 +0100

    i was dumb and it works now :-)

--- buffet-api/tests/BuffetApiTest.php
+++ buffet-api/tests/BuffetApiTest.php
@@ -12,6 +12,7 @@ class BuffetApiTest extends TestCase
 
     protected string $envPath = __DIR__ . "/../src/Database/.env";
     protected string $envDir = __DIR__ . "/../src/Database/";
+    protected string $envContent = "DECRYPT_KEY=a_key";
 
     protected bool $returnOriginalEnv = false;
 
@@ -19,16 +20,11 @@ class BuffetApiTest extends TestCase
     {
         // Set up a mock .env environment for testing
         if (file_exists($this->envPath)) {
-            echo "dsf";
             $this->returnOriginalEnv = true;
             copy($this->envPath, __DIR__ . "/temp.env");
         } else {
-            echo "nope";
-            var_dump(is_writable($this->envDir));
-            var_dump( $this->envPath);
-            //exec("touch " . escapeshellarg($this->envPath), $output, $return);
             if ($env = fopen($this->envPath, "w")) {
-                fwrite($env, "DECRYPT_KEY=12345678901234567890123456789012");
+                fwrite($env, $this->envContent);
                 fclose($env);
             }
         }
@@ -37,7 +33,7 @@ class BuffetApiTest extends TestCase
     #[TestDox('Setup works')]
     public function testRunTest()
     {
-        $this->assertFileExists(__DIR__ . $this->envPath);
+        $this->assertFileExists($this->envPath);
     }
 
     protected function tearDown(): void
