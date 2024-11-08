b640c0b24ca64ab392587f149838040e5c77bc04
Adam Vlček <vlcek-adam@seznam.cz>
2024-11-09 00:57:23 +0100

    fancy config

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker exec -it php-apache /var/www/html/vendor/bin/phpunit tests --testdox --colors=always --coverage-text
+docker exec -it php-apache /var/www/html/vendor/bin/phpunit tests --testdox --colors=always --coverage-text --display-deprecations --display-phpunit-deprecations --fail-on-deprecation --fail-on-phpunit-deprecation
 #docker exec -it php-apache /var/www/html/vendor/bin/phpunit tests --coverage-text
 
 # export XDEBUG_MODE=debug
