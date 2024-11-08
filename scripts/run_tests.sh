bd779d48ab3a5553e29f323a5a30c592c18f9f0a
Adam Vlček <vlcek-adam@seznam.cz>
2024-11-08 22:38:34 +0100

    setup code coverage

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-docker exec -it php-apache /var/www/html/vendor/bin/phpunit tests --testdox --colors=always
+docker exec -it php-apache /var/www/html/vendor/bin/phpunit tests --testdox --colors=always --coverage-text
+#docker exec -it php-apache /var/www/html/vendor/bin/phpunit tests --coverage-text
+
+# export XDEBUG_MODE=debug
