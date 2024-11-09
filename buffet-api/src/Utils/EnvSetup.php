0df270b9ec6ad20dd3f5e8215194dbf251b1878d
Adam Vlček <vlcek-adam@seznam.cz>
2024-11-09 12:27:40 +0100

    updated envSetup && envSetupTest

--- buffet-api/src/Utils/EnvSetup.php
+++ buffet-api/src/Utils/EnvSetup.php
@@ -10,7 +10,8 @@ class EnvSetup
     public string $envPath;
 
     public string $envContent = "DECRYPT_KEY=a_key";
-    protected bool $returnOriginalEnv = false;
+    public bool $returnOriginalEnv = false;
+    public bool $isDummy = false;
 
     public function __construct()
     {
@@ -23,26 +24,32 @@ class EnvSetup
      */
     public function setupDummyEnv($fileContent = ""): void
     {
+        // takes default content if custom content is not provided
         if (empty($fileContent)) {
             $fileContent = $this->envContent;
         }
-        if (file_exists($this->envPath)) {
+
+        if (file_exists($this->envPath) && $this->returnOriginalEnv == false) {
             $this->returnOriginalEnv = true;
             copy($this->envPath, __DIR__ . "/temp.env");
-        } else {
-            if ($env = fopen($this->envPath, "w")) {
-                fwrite($env, $fileContent);
-                fclose($env);
-            }
+        }
+        if ($env = fopen($this->envPath, "w")) {
+            fwrite($env, $fileContent);
+            fclose($env);
+            $this->isDummy = true;
         }
     }
 
     public function cleanupDummyEnv(): void
     {
-        unlink($this->envPath);
+        if ($this->isDummy) {
+            unlink($this->envPath);
+            $this->isDummy = false;
+        }
         if ($this->returnOriginalEnv) {
             copy(__DIR__ . "/temp.env", $this->envPath);
             unlink(__DIR__ . "/temp.env");
+            $this->returnOriginalEnv = false;
         }
     }
 
--- buffet-api/tests/EnvSetupTest.php
+++ buffet-api/tests/EnvSetupTest.php
@@ -24,26 +24,53 @@ class EnvSetupTest extends TestCase
     #[TestDox('Env file creation')]
     public function testEnvSetup()
     {
-        $this->assertFileDoesNotExist($this->envSetup->envPath);
+        $this->assertFalse($this->envSetup->returnOriginalEnv);
+
+        // checks for original env file
+        if (file_exists($this->envSetup->envPath)) {
+            $hadOrginalEnv = true;
+        }
 
         $this->envSetup->setupDummyEnv();
+
+        if ($hadOrginalEnv) {
+            $this->assertTrue($this->envSetup->returnOriginalEnv);
+        }
+
         $this->assertFileExists($this->envSetup->envPath);
         $this->assertFileIsReadable($this->envSetup->envPath);
         $this->assertFileIsWritable($this->envSetup->envPath);
         $this->assertStringEqualsFile(expectedFile: $this->envSetup->envPath, actualString: $this->envSetup->envContent, message: "Created env file content does not match the default value");
-
-        $this->envSetup->cleanupDummyEnv();
-        $this->assertFileDoesNotExist($this->envSetup->envPath);
     }
 
     #[TestDox('Env file custom content')]
     public function testEnvFileContent()
     {
-        $customEnvContent = 'FOO=BAR';
+        $customEnvContent = 'CUSTOM=CONTENT';
         $this->envSetup->setupDummyEnv($customEnvContent);
 
         $this->assertFileExists($this->envSetup->envPath);
         $this->assertFileIsReadable($this->envSetup->envPath);
+        $this->assertStringEqualsFile(expectedFile: $this->envSetup->envPath, actualString: $customEnvContent, message: "Created env file content does not match the custom value");
+    }
+
+    #[TestDox('Env file backup')]
+    public function testEnvFileBackup()
+    {
+        $customEnvContent = 'CUSTOM=CONTENT';
+
+        // checks for original env file
+        if (file_exists($this->envSetup->envPath)) {
+            $hadOrginalEnv = true;
+        }
+
+        $this->assertFalse($this->envSetup->returnOriginalEnv);
+
+        $this->envSetup->setupDummyEnv($customEnvContent);
+
+        if ($hadOrginalEnv) {
+            $this->assertTrue($this->envSetup->returnOriginalEnv);
+        }
         $this->assertStringEqualsFile(expectedFile: $this->envSetup->envPath, actualString: $customEnvContent, message: "Created env file content does not match the custom value");
     }
 
