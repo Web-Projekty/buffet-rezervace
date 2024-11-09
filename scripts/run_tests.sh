89378d567504f743cc8d60f758605b78ed359123
Adam Vlček <vlcek-adam@seznam.cz>
2024-11-09 01:38:47 +0100

    clear terminal after each test

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+clear
 docker exec -it php-apache /var/www/html/vendor/bin/phpunit tests --testdox --colors=always --coverage-text --display-deprecations --display-phpunit-deprecations --fail-on-deprecation --fail-on-phpunit-deprecation
 #docker exec -it php-apache /var/www/html/vendor/bin/phpunit tests --coverage-text
 
