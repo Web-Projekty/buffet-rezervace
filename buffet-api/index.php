2875cd87d3a65c3dc12859b15e15bb9c13c32381
Adam Vlček <vlcek-adam@seznam.cz>
2024-11-17 16:17:12 +0100

    test version of ws server

--- buffet-api/index.php
+++ buffet-api/index.php
@@ -64,4 +64,12 @@ $app->post('/api', [BuffetApi::class, 'main']);
 
 $app->any('/image/{path:.*}', [ImageProvider::class, 'main']);
 
+$app->get('/wstest', function (Request $request, Response $response, $args) {
+
+    ob_start();
+    include __DIR__ . "/templates/ws.html";
+    $response->getBody()->write(ob_get_clean());
+    return $response;
+});
+
 $app->run();
---
+++ buffet-api/src/WebSockets/server.php
@@ -0,0 +1,85 @@
+<?php
+
+require __DIR__ . '/../../vendor/autoload.php';
+
+use Ratchet\ConnectionInterface;
+use Ratchet\MessageComponentInterface;
+
+class ChatServer implements MessageComponentInterface
+{
+    /**
+     * @var mixed
+     */
+    private $clients;
+
+    public function __construct()
+    {
+        $this->clients = new \SplObjectStorage; // Store connected clients
+    }
+
+    /**
+     * @param ConnectionInterface $conn
+     */
+    public function onOpen(ConnectionInterface $conn)
+    {
+        // Store the new connection
+        $this->clients->attach($conn);
+        echo "New connection! ({$conn->resourceId})\n";
+    }
+
+    /**
+     * @param ConnectionInterface $from
+     * @param $msg
+     */
+    public function onMessage(ConnectionInterface $from, $msg)
+    {
+        $numRecv = count($this->clients) - 1;
+        echo sprintf('Connection %d sending message "%s" to %d other connection%s' . "\n",
+            $from->resourceId, $msg, $numRecv, $numRecv == 1 ? '' : 's');
+
+        // Broadcast the message to all clients
+        foreach ($this->clients as $client) {
+            if ($from !== $client) { // Skip the sender
+                $client->send($msg);
+            }
+        }
+    }
+
+    /**
+     * @param ConnectionInterface $conn
+     */
+    public function onClose(ConnectionInterface $conn)
+    {
+        // Remove the connection
+        $this->clients->detach($conn);
+        echo "Connection {$conn->resourceId} has disconnected\n";
+    }
+
+    /**
+     * @param ConnectionInterface $conn
+     * @param \Exception          $e
+     */
+    public function onError(ConnectionInterface $conn, \Exception $e)
+    {
+        echo "An error has occurred: {$e->getMessage()}\n";
+        $conn->close();
+    }
+}
+
+// Run the server
+use Ratchet\Server\IoServer;
+use Ratchet\Http\HttpServer;
+use Ratchet\WebSocket\WsServer;
+
+$server = IoServer::factory(
+    new HttpServer(
+        new WsServer(
+            new ChatServer()
+        )
+    ),
+    8069// Port to listen on
+);
+
+echo "WebSocket server started on ws://localhost:8080\n";
+
+$server->run();
