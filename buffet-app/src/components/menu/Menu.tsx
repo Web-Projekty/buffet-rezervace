56226bc4e6a566782955ea303de1ef10394560d8
ptakondrej <ptakondrej@spseplzen.cz>
2024-11-17 21:36:34 +0100

    Add more columns for menu

--- buffet-app/src/components/menu/Menu.tsx
+++ buffet-app/src/components/menu/Menu.tsx
@@ -37,7 +37,7 @@ const Menu = () => {
     <div className="flex flex-col items-center justify-center gap-5">
       <h1 className="text-3xl font-bold text-white">Menu</h1>
       {/* {isLoading && <Loading size={30} />} */}
-      <div className="grid grid-cols-1 gap-10 md:grid-cols-2 xl:grid-cols-3">
+      <div className="grid grid-cols-1 gap-10 md:grid-cols-2 xl:grid-cols-4">
         {dataList.map((item) => {
           return <MenuItem key={item.id} item={item} />;
         })}
--- buffet-app/src/constants.ts
+++ buffet-app/src/constants.ts
@@ -2,7 +2,7 @@ export const maxItems: number = 5;
 
 export const maxItemsInCart: number = 10;
 
-export const itemsPerPage: number = 9;
+export const itemsPerPage: number = 12;
 
 export const adminOrdersPerPage: number = 10;
 
