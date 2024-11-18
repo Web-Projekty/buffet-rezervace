d61c1135cf5d98b618e86b99e642a567b6e5ab6d
ptakondrej <ptakondrej@spseplzen.cz>
2024-11-18 10:29:41 +0100

    Fetching backend menu

--- buffet-app/src/components/menu/Menu.tsx
+++ buffet-app/src/components/menu/Menu.tsx
@@ -1,13 +1,12 @@
 import { useEffect, useState } from "react";
-import { dummyFood } from "../../dummyData";
 import { usePaging } from "../../hooks/usePaging";
 import PagingButtons from "../PagingButtons";
 import MenuItem from "./MenuItem";
 import { MenuItem as MenuItemType } from "../../types";
 import useFetch from "../../hooks/useFetch";
-import useAuthHeader from "react-auth-kit/hooks/useAuthHeader";
 import Loading from "../Loading";
 import { itemsPerPage } from "../../constants";
+import ErrorComponent from "../error/ErrorComponent";
 
 const Menu = () => {
   const [menu, setMenu] = useState<MenuItemType[]>([]);
@@ -15,23 +14,28 @@ const Menu = () => {
   const { currentPage, totalPagesCount, dataList, arrayOfPages, handlePage } =
     usePaging<MenuItemType>(menu, itemsPerPage);
 
-  // const { data, isLoading, error } = useFetch<MenuItemType[]>(
-  //   "https://wlczak.vlastas.cc/backend/api",
-  //   { requestType: "getMenu" },
-  //   [],
-  // );
-
-  useEffect(
-    () => {
-      // if (data) {
-      //   setMenu(data);
-      // }
-      setMenu(dummyFood as MenuItemType[]);
-    },
-    [
-      /*data*/
-    ],
+  const { data, isLoading, error } = useFetch<MenuItemType[]>(
+    "https://wlczak.vlastas.cc/backend/api",
+    { requestType: "getMenu", page: 1, itemsCount: itemsPerPage },
+    [],
   );
+
+  useEffect(() => {
+    if (data) {
+      setMenu(data);
+    }
+    //setMenu(dummyFood as MenuItemType[]);
+  }, []);
+
+  if (isLoading) {
+    return <Loading size={30} />;
+  }
+
+  if (error) {
+    return (
+      <ErrorComponent title="Načítání položek se nezdařilo." subtitle="🛠️👷" />
+    );
+  }
 
   return (
     <div className="flex flex-col items-center justify-center gap-5">
--- buffet-app/src/hooks/useFetch.ts
+++ buffet-app/src/hooks/useFetch.ts
@@ -8,14 +8,9 @@ type UseFetchReturn<T> = {
   data: T | null;
 };
 
-type RequestData = {
-  requestType: string;
-  token?: string | undefined;
-};
-
 const useFetch = <T>(
   url: string,
-  requestData: RequestData,
+  requestData: Record<string, unknown>,
   initialValue?: T,
 ): UseFetchReturn<T> => {
   const [isLoading, setIsLoading] = useState<boolean>(false);
@@ -30,7 +25,8 @@ const useFetch = <T>(
     async function fetchData() {
       try {
         const { data } = await axios.post(url, requestData);
-        setData(data as T);
+        console.log(data.payload);
+        setData(data.payload.menuItems as T);
       } catch (e) {
         console.log(e);
         setError("Chyba načítání dat ze serveru.");
