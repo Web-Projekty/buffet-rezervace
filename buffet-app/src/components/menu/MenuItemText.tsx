d0b7763e4855799c36e0a5e785a90fca342ec340
ptakondrej <ptakondrej@spseplzen.cz>
2024-11-18 21:05:57 +0100

    Update menu item font

--- buffet-app/src/components/menu/MenuItemText.tsx
+++ buffet-app/src/components/menu/MenuItemText.tsx
@@ -9,15 +9,15 @@ type MenuItemTextProps = {
 const MenuItemText = ({ name, price, description }: MenuItemTextProps) => {
   return (
     <>
-      <div className="flex items-center font-bold">
-        <h1 className="rounded-lg px-2 text-xl">{name}</h1>
+      <div className="flex items-center font-FiraSans font-bold">
+        <h1 className="rounded-lg px-2 font-SourGummy text-xl">{name}</h1>
         <div className="mt-4 flex-1 border-b-2 border-dotted border-white"></div>
-        <p className="font-FiraSans rounded-lg px-2 text-lg font-normal italic">
+        <p className="rounded-lg px-2 text-lg font-normal italic">
           {formatCurrency(price)}
         </p>
       </div>
 
-      <div className="font-FiraSans flex flex-col items-start px-2">
+      <div className="flex flex-col items-start px-2">
         <p className="rounded-lg">{description}</p>
       </div>
     </>
