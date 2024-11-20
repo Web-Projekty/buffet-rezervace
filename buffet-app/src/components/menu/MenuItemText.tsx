66ef13e399024f32c209129ed8a2fe8e41d7e82e
ptakondrej <ptakondrej@spseplzen.cz>
2024-11-20 22:05:32 +0100

    Update menu item style

--- buffet-app/src/components/menu/MenuItemText.tsx
+++ buffet-app/src/components/menu/MenuItemText.tsx
@@ -10,8 +10,8 @@ const MenuItemText = ({ name, price, description }: MenuItemTextProps) => {
   return (
     <>
       <div className="flex items-center font-FiraSans font-bold">
-        <h1 className="rounded-lg px-2 font-SourGummy text-xl">{name}</h1>
-        <div className="mt-4 flex-1 border-b-2 border-dotted border-white"></div>
+        <h1 className="rounded-lg px-1 font-SourGummy text-xl">{name}</h1>
+        <div className="mt-4 flex-1 border-b-2 border-dotted border-white px-2"></div>
         <p className="rounded-lg px-2 text-lg font-normal italic">
           {formatCurrency(price)}
         </p>
