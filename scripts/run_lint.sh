24bf1a07b1c71173112ac2310b7b460ac2cabc11
Adam Vlček <vlcek-adam@seznam.cz>
2024-11-24 19:41:24 +0100

    Update run_lint.sh

--- scripts/run_lint.sh
+++ scripts/run_lint.sh
@@ -1,2 +1,2 @@
 clear
-docker exec -it php-apache /var/www/html/vendor/bin/phpstan analyse --autoload-file vendor/autoload.php --level 1 src tests
+docker exec -it php-apache /var/www/html/vendor/bin/phpstan analyse -c phpstan.neon --autoload-file vendor/autoload.php src tests
