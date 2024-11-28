ab832d2e4f380acb00c7f86d0f84b3c91a766566
Adam Vlček <vlcek-adam@seznam.cz>
2024-11-28 13:16:04 +0100

    added static declaration of conn object properties

---
+++ buffet-api/src/WebSockets/StaticConnectionInterface.php
@@ -0,0 +1,55 @@
+<?php
+namespace Buffet\WebSockets;
+
+class StaticConnectionInterface
+{
+    /**
+     * @var mixed
+     */
+    public $resourceId;
+    /**
+     * @var mixed
+     */
+    public $remoteAddress;
+    /**
+     * @var mixed
+     */
+    public $httpHeadersReceived;
+
+    /**
+     * @var mixed
+     */
+    public $httpRequest;
+    /**
+     * @var mixed
+     */
+    public $WebSocket;
+
+    /**
+     * @var mixed
+     */
+    private $original;
+
+    /**
+     * @param $original - original object
+     */
+    public function __construct($original)
+    {
+        $this->original = $original;
+        $this->resourceId = $original->resourceId;
+        $this->remoteAddress = $original->remoteAddress;
+        $this->httpHeadersReceived = $original->httpHeadersReceived;
+        $this->httpRequest = $original->httpRequest;
+        $this->WebSocket = $original->WebSocket;
+    }
+
+    /**
+     * @param  $method
+     * @param  $arguments
+     * @return mixed
+     */
+    public function __call($method, $arguments)
+    {
+        return $this->original->$method(...$arguments);
+    }
+}
