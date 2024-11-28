68f06c5233c31d3e0d64be316672be4b402940b9
Adam Vlček <vlcek-adam@seznam.cz>
2024-11-28 19:33:06 +0100

    fixed error parameter types

--- buffet-api/src/Api/ImageProvider.php
+++ buffet-api/src/Api/ImageProvider.php
@@ -4,9 +4,8 @@ declare (strict_types = 1);
 
 namespace Buffet\Api;
 
-use PHPUnit\Runner\FileDoesNotExistException;
-use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface as RequestInterface;
 use Slim\Exception\HttpNotFoundException;
 
 class ImageProvider
@@ -19,11 +18,7 @@ class ImageProvider
     function main(RequestInterface $request, ResponseInterface $html, $args): ResponseInterface
     {
         $urlPath = $args['path'];
-        $path = $this->getFilePath($urlPath);
-
-        if (!file_exists($path)) {
-            throw new HttpNotFoundException($request);
-        }
+        $path = $this->getFilePath($urlPath, $request);
 
         if (explode("/", mime_content_type($path))[0] != "image") {
             throw new HttpNotFoundException($request, "not an image");
@@ -32,14 +27,14 @@ class ImageProvider
         $html->getBody()->write(file_get_contents($path));
 
         return $html->withHeader('Content-Type', mime_content_type($path))
-            ->withHeader('Content-Length', filesize($path));
+            ->withHeader('Content-Length', (string) filesize($path));
     }
 
     /**
      * @param  $args
      * @return string
      */
-    function getFilePath($urlPath): string
+    function getFilePath($urlPath, RequestInterface $request): string
     {
         $supportedFormats = ["jpg", "png", "svg", "webp"];
         $fileFound = false;
@@ -73,6 +68,6 @@ class ImageProvider
             }
 
         }
-        return $fileFound ? $path : throw (new FileDoesNotExistException($path));
+        return $fileFound ? $path : throw (new HttpNotFoundException($request, "file not found: " . $path));
     }
 }
