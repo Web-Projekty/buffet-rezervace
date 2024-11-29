0d145f2ca8d8ccf75b8a8d2926e29a7197249e7f
ptakondrej <ptakondrej@spseplzen.cz>
2024-11-29 18:49:33 +0100

    Move allergens to own file

--- buffet-app/src/dummyData.ts
+++ buffet-app/src/dummyData.ts
@@ -1,4 +1,4 @@
-import { Allergen, Order, User } from "./types";
+import { Order, User } from "./types";
 
 export const dummyUser: User = {
   id: 1,
@@ -404,56 +404,3 @@ export const dummyOrders: Order[] = [
   dummyUser2.orders,
   dummyUser3.orders,
 ].flat();
-
-export const allergens: Allergen[] = [
-  {
-    id: 1,
-    name: "Lepek",
-    description: "Tento produkt obsahuje lepek",
-  },
-  {
-    id: 2,
-    name: "Mléko",
-    description: "Tento produkt obsahuje mléko",
-  },
-  {
-    id: 3,
-    name: "Vejce",
-    description: "Tento produkt obsahuje vejce",
-  },
-  {
-    id: 4,
-    name: "Ořechy",
-    description: "Tento produkt obsahuje ořechy",
-  },
-  {
-    id: 5,
-    name: "Arašídové ořechy",
-    description: "Tento produkt obsahuje arašídy",
-  },
-  {
-    id: 6,
-    name: "Ryby",
-    description: "Tento produkt obsahuje ryby",
-  },
-  {
-    id: 7,
-    name: "Korýši",
-    description: "Tento produkt obsahuje korýše",
-  },
-  {
-    id: 8,
-    name: "Sója",
-    description: "Tento produkt obsahuje sóju",
-  },
-  {
-    id: 9,
-    name: "Sezam",
-    description: "Tento produkt obsahuje sezam",
-  },
-  {
-    id: 10,
-    name: "Celer",
-    description: "Tento produkt obsahuje celer",
-  },
-];
