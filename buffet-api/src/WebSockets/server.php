b0de987b1770f4ec50164804888c84465a01e020
Adam Vlček <vlcek-adam@seznam.cz>
2024-11-29 16:04:55 +0100

    added routing

--- buffet-api/src/WebSockets/server.php
+++ buffet-api/src/WebSockets/server.php
@@ -4,23 +4,14 @@ declare (strict_types = 1);
 
 namespace Buffet\WebSockets;
 
-use Buffet\WebSockets\Channel;
-use Ratchet\Http\HttpServer;
-use Ratchet\Server\IoServer;
-use Ratchet\WebSocket\WsServer;
+use Ratchet\App;
 
 error_reporting(E_ALL & ~E_DEPRECATED & ~E_USER_DEPRECATED);
 
 require dirname(__DIR__) . '/../vendor/autoload.php';
 
-$server = IoServer::factory(
-    new HttpServer(
-        new WsServer(
-            new Channel()
-        )
-    )
-    ,
-    8069
-);
+$server = new App("localhost", 8080);
+
+$server->route('/backend', new Channel());
 
 $server->run();
