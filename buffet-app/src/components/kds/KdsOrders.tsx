f0e034996ffcb14ffd4912a9eddfb43fd864f435
ptakondrej <ptakondrej@spseplzen.cz>
2024-11-29 18:46:17 +0100

    Update orders loading

--- buffet-app/src/components/kds/KdsOrders.tsx
+++ buffet-app/src/components/kds/KdsOrders.tsx
@@ -3,8 +3,10 @@ import useFetch from "../../hooks/useFetch";
 import { Order } from "../../types";
 import KdsDeliveryOrder from "./KdsDeliveryOrder";
 import KdsOrder from "./KdsOrder";
-import KdsStatusCards from "./KdsStatusCards";
+
 import { usePaging } from "../../hooks/usePaging";
+import Loading from "../Loading";
+import KdsStatusBar from "./KdsStatusBar";
 
 const KdsOrders = () => {
   const token = useAuthHeader()?.split(" ")[1];
@@ -15,65 +17,45 @@ const KdsOrders = () => {
     [token],
   );
 
-  const { dataList } = usePaging<Order>(data, 5);
-
-  if (isLoading) {
-    return <div className="text-white">Loading...</div>;
-  }
-
-  if (error) {
-    return <div className="text-white">Error: {error}</div>;
-  }
+  const { dataList: pendingOrders } = usePaging<Order>(
+    data?.filter((order) => order.status !== "pickedup"),
+    8,
+  );
+  const { dataList: pickedUpOrders } = usePaging<Order>(
+    data?.filter((order) => order.status === "pickedup"),
+    5,
+  );
 
   return (
     <div className="mx-auto flex w-[85.5%] flex-col justify-center">
-      {/* KDS Orders Status */}
-      <div className="my-2 flex h-[5rem] w-full items-center justify-between bg-white px-10">
-        <div className="flex flex-row items-center gap-10">
-          <KdsStatusCards
-            amount={1}
-            title="Zpožděné"
-            backgroundColor="bg-red-400"
-          />
-          <KdsStatusCards
-            amount={3}
-            title="Aktuální"
-            backgroundColor="bg-orange-400"
-          />
-          <KdsStatusCards
-            amount={2}
-            title="Nadcházející"
-            backgroundColor="bg-yellow-400"
-          />
-        </div>
-        <KdsStatusCards
-          amount={6}
-          title="Výdej"
-          backgroundColor="bg-green-400"
-        />
-      </div>
+      <KdsStatusBar />
 
-      {/* KDS Orders */}
-      <div className="flex flex-row items-start justify-between">
-        <div className="flex flex-wrap gap-2">
-          {dataList ? (
-            dataList.map((order, index) => (
-              <KdsOrder key={index} order={order} />
-            ))
-          ) : (
-            <p className="text-4xl">No orders available</p>
-          )}
+      {isLoading ? (
+        <Loading size={30} />
+      ) : error ? (
+        <p className="text-center text-4xl text-white">{error}</p>
+      ) : (
+        <div className="flex flex-row items-start justify-between">
+          <div className="flex flex-wrap gap-2">
+            {pendingOrders && pendingOrders.length > 0 ? (
+              pendingOrders.map((order, index) => (
+                <KdsOrder key={index} order={order} />
+              ))
+            ) : (
+              <p className="text-4xl text-white">
+                Žádné objevnávky nejsou dostupné
+              </p>
+            )}
+          </div>
+          <div className="flex flex-col gap-2">
+            {pickedUpOrders && !(pickedUpOrders.length < 1)
+              ? pickedUpOrders.map((order, index) => (
+                  <KdsDeliveryOrder key={index} order={order} />
+                ))
+              : null}
+          </div>
         </div>
-        <div className="flex flex-col gap-2">
-          {dataList ? (
-            dataList.map((order, index) => (
-              <KdsDeliveryOrder key={index} order={order} />
-            ))
-          ) : (
-            <p className="text-4xl">No orders available</p>
-          )}
-        </div>
-      </div>
+      )}
     </div>
   );
 };
