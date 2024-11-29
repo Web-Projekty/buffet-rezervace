a2248d18a6879e63aa0c7b5412312edeabbdc90b
ptakondrej <ptakondrej@spseplzen.cz>
2024-11-29 18:49:09 +0100

    Move util to other file

--- buffet-app/src/components/utils/utils.ts
+++
@@ -1,9 +0,0 @@
-import { OrderStatus } from "../../types";
-
-export const getColorByStatus = (status: OrderStatus): string => {
-  return status === "pickedup"
-    ? "bg-green-500"
-    : status === "notpickedup"
-      ? "bg-red-500"
-      : "bg-orange-400";
-};
