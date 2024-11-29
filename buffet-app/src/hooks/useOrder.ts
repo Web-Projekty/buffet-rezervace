40e357c30141578d1e99f7af0e3bdae7b7e8a83b
ptakondrej <ptakondrej@spseplzen.cz>
2024-11-29 18:49:55 +0100

    Update imports

--- buffet-app/src/hooks/useOrder.ts
+++ buffet-app/src/hooks/useOrder.ts
@@ -1,26 +1,33 @@
 import { useEffect, useState } from "react";
-import { Order } from "../types";
-import { getColorByStatus } from "../components/utils/utils";
+import { Order, OrderStatus } from "../types";
+import { getColorByStatus } from "../utils";
 
 type UseOrderReturn = {
   isOpen: boolean;
   toggleOpen: () => void;
   color: string;
+  status: OrderStatus;
+  handleStatus: (status: OrderStatus) => void;
 };
 
 const useOrder = (order: Order): UseOrderReturn => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
   const [color, setColor] = useState<string>("bg-white");
+  const [status, setStatus] = useState<OrderStatus>("pending");
 
   const toggleOpen = () => {
     setIsOpen(!isOpen);
   };
 
+  const handleStatus = (status: OrderStatus) => {
+    setStatus(status);
+  };
+
   useEffect(() => {
-    setColor(getColorByStatus(order.status));
+    setColor(getColorByStatus(status));
   }, [order]);
 
-  return { isOpen, toggleOpen, color };
+  return { isOpen, toggleOpen, color, status, handleStatus };
 };
 
 export default useOrder;
