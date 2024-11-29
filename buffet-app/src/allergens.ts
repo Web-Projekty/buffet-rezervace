b2d7c6b1dcc17e2a0d4a3c495c85631ab488b3e9
ptakondrej <ptakondrej@spseplzen.cz>
2024-11-29 18:03:53 +0100

    Create allergen list

---
+++ buffet-app/src/allergens.ts
@@ -0,0 +1,74 @@
+import { Allergen } from "./types";
+
+export const allergens: Allergen[] = [
+  {
+    id: 1,
+    name: "Lepek",
+    description: "Tento produkt obsahuje lepek",
+  },
+  {
+    id: 2,
+    name: "Mléko",
+    description: "Tento produkt obsahuje mléko",
+  },
+  {
+    id: 3,
+    name: "Vejce",
+    description: "Tento produkt obsahuje vejce",
+  },
+  {
+    id: 4,
+    name: "Ořechy",
+    description: "Tento produkt obsahuje ořechy",
+  },
+  {
+    id: 5,
+    name: "Arašídové ořechy",
+    description: "Tento produkt obsahuje arašídy",
+  },
+  {
+    id: 6,
+    name: "Ryby",
+    description: "Tento produkt obsahuje ryby",
+  },
+  {
+    id: 7,
+    name: "Korýši",
+    description: "Tento produkt obsahuje korýše",
+  },
+  {
+    id: 8,
+    name: "Sója",
+    description: "Tento produkt obsahuje sóju",
+  },
+  {
+    id: 9,
+    name: "Sezam",
+    description: "Tento produkt obsahuje sezam",
+  },
+  {
+    id: 10,
+    name: "Celer",
+    description: "Tento produkt obsahuje celer",
+  },
+  {
+    id: 11,
+    name: "Hořčice",
+    description: "Tento produkt obsahuje hořčici",
+  },
+  {
+    id: 12,
+    name: "Oxid siřičitý a siřičitany",
+    description: "Tento produkt obsahuje oxid siřičitý a siřičitany",
+  },
+  {
+    id: 13,
+    name: "Vlčí bob",
+    description: "Tento produkt obsahuje vlčí bob",
+  },
+  {
+    id: 14,
+    name: "Měkkýši",
+    description: "Tento produkt obsahuje měkkýše",
+  },
+];
