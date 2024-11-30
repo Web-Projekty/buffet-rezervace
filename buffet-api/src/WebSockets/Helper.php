3c49eb3a9accfd41a72e08b30cccc840e286aabb
Adam Vlček <vlcek-adam@seznam.cz>
2024-11-30 22:05:57 +0100

    switched to static methods + new methods

--- buffet-api/src/WebSockets/Helper.php
+++ buffet-api/src/WebSockets/Helper.php
@@ -4,16 +4,20 @@ declare (strict_types = 1);
 
 namespace Buffet\WebSockets;
 
+use Buffet\Api\JWTApi;
+use Buffet\Database\Models\UserModel;
 use Buffet\Types\ApiResponse;
 use Buffet\Types\Error;
 use Buffet\Types\Success;
+use Buffet\WebSockets\Interfaces\StaticConnectionInterface;
+use SplObjectStorage;
 
 class Helper
 {
     /**
      * @param Error $error
      */
-    public function getErrorResponse(Error $error): string
+    public static function getErrorResponse(Error $error): string
     {
         $response = new ApiResponse();
         return (string) $response->requireRequestType(false)->setError($error);
@@ -22,9 +26,67 @@ class Helper
     /**
      * @param Success $success
      */
-    public function getSuccessResponse(Success $success): string
+    public static function getSuccessResponse(Success $success): string
     {
         $response = new ApiResponse();
         return (string) $response->requireRequestType(false)->setSuccess($success);
+    }
+
+    /**
+     * @param  string $token
+     * @return bool
+     */
+    public static function isAdmin(string $token): bool
+    {
+        $response = new ApiResponse(["token" => $token]);
+        $jwtApi = new JWTApi;
+
+        $response->requireRequestType(false);
+        $jwtApi->validateToken($response);
+        if ($response->hasFailed()) {
+            return false;
+        }
+        $uid = $jwtApi->decodeToken($response)->sub ?? 0;
+
+        return UserModel::isAdmin($uid);
+    }
+
+    /**
+     * @param StaticConnectionInterface                        $conn
+     * @param SplObjectStorage<StaticConnectionInterface,null> $storage
+     */
+    public static function attachClient(StaticConnectionInterface $conn, SplObjectStorage $storage): void
+    {
+        if (!self::isClientInStorage($conn, $storage)) {
+            $storage->attach($conn);
+        }
+    }
+
+    /**
+     * @param StaticConnectionInterface                        $conn
+     * @param SplObjectStorage<StaticConnectionInterface,null> $storage
+     */
+    public static function isClientInStorage(StaticConnectionInterface $conn, SplObjectStorage $storage): bool
+    {
+        foreach ($storage as $client) {
+            if ($client->resourceId == $conn->resourceId) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * @param StaticConnectionInterface                        $conn
+     * @param SplObjectStorage<StaticConnectionInterface,null> $storage
+     */
+    public static function removeClient(StaticConnectionInterface $conn, SplObjectStorage $storage): void
+    {
+        foreach ($storage as $client) {
+            if ($client->resourceId == $conn->resourceId) {
+                $storage->detach($client);
+                break;
+            }
+        }
     }
 }
