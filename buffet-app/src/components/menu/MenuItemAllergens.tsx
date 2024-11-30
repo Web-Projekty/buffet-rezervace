a86f1dd4daf1e3acc7012a68fbfc99f54fe3acf0
ptakondrej <ptakondrej@spseplzen.cz>
2024-11-30 21:23:06 +0100

    Add title to menu item allergen

--- buffet-app/src/components/menu/MenuItemAllergens.tsx
+++ buffet-app/src/components/menu/MenuItemAllergens.tsx
@@ -1,4 +1,5 @@
 import { Allergen } from "../../types";
+import { allergens as AllergensList } from "../../allergens";
 
 type MenuItemAllergensProps = {
   allergens: Allergen[];
@@ -8,7 +9,11 @@ const MenuItemAllergens = ({ allergens }: MenuItemAllergensProps) => {
   return (
     <ul className="absolute bottom-0 m-1 flex flex-row gap-1">
       {allergens.map((allergen) => (
-        <li key={allergen.id} className="rounded-full bg-slate-600 p-1 px-3">
+        <li
+          key={allergen.id}
+          className="rounded-full bg-slate-600 p-1 px-3"
+          title={AllergensList.find((a) => a.id === allergen.id)?.description}
+        >
           {allergen.id}
         </li>
       ))}
