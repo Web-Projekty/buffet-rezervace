5e0bc2dac654f9fc402b17b3133c57760753fc75
ptakondrej <ptakondrej@spseplzen.cz>
2024-12-01 22:10:32 +0100

    Add link to to kds buttons

--- buffet-app/src/components/kds/KdsButton.tsx
+++ buffet-app/src/components/kds/KdsButton.tsx
@@ -1,4 +1,11 @@
+import { Link } from "react-router-dom";
 import { ButtonProps } from "../Button";
+
+type KdsLinkTo = {
+  linkTo: "souhrn" | "objednávky" | "uprava-menu";
+};
+
+type KdsButtonProps = ButtonProps & { linkTo: KdsLinkTo["linkTo"] };
 
 const KdsButton = ({
   type,
@@ -6,16 +13,19 @@ const KdsButton = ({
   className,
   disabled,
   children,
-}: ButtonProps) => {
+  linkTo,
+}: KdsButtonProps) => {
   return (
-    <button
-      type={type}
-      onClick={onClick}
-      disabled={disabled}
-      className={`w-36 rounded-lg bg-blue-500 px-4 py-2 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300 disabled:bg-gray-400 ${className}`}
-    >
-      {children}
-    </button>
+    <Link to={linkTo}>
+      <button
+        type={type}
+        onClick={onClick}
+        disabled={disabled}
+        className={`w-36 bg-blue-500 px-4 py-2 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300 disabled:bg-gray-400 ${className}`}
+      >
+        {children}
+      </button>
+    </Link>
   );
 };
 
--- buffet-app/src/components/kds/KdsNavbar.tsx
+++ buffet-app/src/components/kds/KdsNavbar.tsx
@@ -3,9 +3,9 @@ import KdsButton from "./KdsButton";
 const KdsNavbar = () => {
   return (
     <nav className="flex items-center justify-center gap-8">
-      <KdsButton onClick={() => {}}>Souhrn</KdsButton>
-      <KdsButton onClick={() => {}}>Objednávky</KdsButton>
-      <KdsButton onClick={() => {}}>Úprava jídel</KdsButton>
+      <KdsButton linkTo="souhrn">Souhrn</KdsButton>
+      <KdsButton linkTo="objednavky">Objednávky</KdsButton>
+      <KdsButton linkTo="uprava-menu">Úprava jídel</KdsButton>
     </nav>
   );
 };
