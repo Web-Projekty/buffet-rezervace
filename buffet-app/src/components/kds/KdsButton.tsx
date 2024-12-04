b1d8b02c7db5cbc483a0423009a38fc2ecf13a19
ptakondrej <ptakondrej@spseplzen.cz>
2024-12-04 08:04:39 +0100

    Update link active style

--- buffet-app/src/components/kds/KdsButton.tsx
+++ buffet-app/src/components/kds/KdsButton.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { ButtonProps } from "../Button";
 
 type KdsLinkTo = {
@@ -16,16 +16,25 @@ const KdsButton = ({
   linkTo,
 }: KdsButtonProps) => {
   return (
-    <Link to={linkTo}>
+    <NavLink
+      className={({ isActive, isPending }) =>
+        isPending
+          ? "bg-blue-200"
+          : isActive
+            ? "cursor-default bg-blue-400"
+            : "bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300"
+      }
+      to={linkTo}
+    >
       <button
         type={type}
         onClick={onClick}
         disabled={disabled}
-        className={`w-36 bg-blue-500 px-4 py-2 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300 disabled:bg-gray-400 ${className}`}
+        className={`w-36 px-4 py-2 text-white disabled:bg-gray-400 ${className}`}
       >
         {children}
       </button>
-    </Link>
+    </NavLink>
   );
 };
 
