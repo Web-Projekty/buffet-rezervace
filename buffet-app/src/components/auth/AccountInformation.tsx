612c6adc32c0d034083c5b8834fa346f744f53e3
ptakondrej <ptakondrej@spseplzen.cz>
2024-12-09 12:31:49 +0100

    Edit passed props

--- buffet-app/src/components/auth/AccountInformation.tsx
+++ buffet-app/src/components/auth/AccountInformation.tsx
@@ -1,33 +1,23 @@
 import useSignOut from "react-auth-kit/hooks/useSignOut";
 import { useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
-import { User } from "../../types";
 import { scaleUpAnimation } from "../../animations";
 import { removeTokenExpiration } from "./login/login";
 import Button from "../Button";
 
 type AccountInformationProps = {
-  user: User;
+  isAdmin: boolean;
+  fullName: string;
+  email: string;
+  class: string;
 };
 
-type AdminButtonsProps = {
-  handleMenuEdit: () => void;
-};
-
-const AdminButtons = ({ handleMenuEdit }: AdminButtonsProps) => {
-  return (
-    <div className="grid grid-cols-1 gap-5 md:grid-cols-2">
-      <button
-        className="rounded-md border bg-cyan-500 p-2 text-white hover:bg-cyan-700"
-        onClick={handleMenuEdit}
-      >
-        Upravit menu
-      </button>
-    </div>
-  );
-};
-
-const AccountInformation = ({ user }: AccountInformationProps) => {
+const AccountInformation = ({
+  isAdmin,
+  fullName,
+  email,
+  class: userClass,
+}: AccountInformationProps) => {
   const logout = useSignOut();
   const navigate = useNavigate();
 
@@ -37,26 +27,19 @@ const AccountInformation = ({ user }: AccountInformationProps) => {
     navigate("/login");
   };
 
-  const handleMenuEdit = () => {
-    navigate("/menu/edit");
-  };
-
   return (
     <div className="flex h-[220px] w-[22rem] flex-col justify-center gap-5 md:w-[300px]">
-      <h1 className="text-2xl">
-        {user.isAdmin ? "Administrátor" : "Uživatel"}
-      </h1>
+      <h1 className="text-2xl">{isAdmin ? "Administrátor" : "Uživatel"}</h1>
       <motion.div
         {...scaleUpAnimation(0.5)}
         className="rounded-md bg-slate-900 p-4"
       >
-        <h2 className="text-2xl font-bold">{user.fullName}</h2>
-        <p className="text-lg">Email: {user.email}</p>
-        <p className="text-lg">Třída: {user.class}</p>
+        <h2 className="text-2xl font-bold">{fullName}</h2>
+        <p className="text-lg">Email: {email}</p>
+        <p className="text-lg">Třída: {userClass}</p>
       </motion.div>
 
       <Button onClick={handleLogout}>Odhlásit</Button>
-      {user.isAdmin && <AdminButtons handleMenuEdit={handleMenuEdit} />}
     </div>
   );
 };
