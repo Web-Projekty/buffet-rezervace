b692f70b1272e95abcb3af315e03e5a6b5908290
ptakondrej <ptakondrej@spseplzen.cz>
2024-12-09 12:42:57 +0100

    Add websocket url

--- buffet-app/src/constants.ts
+++ buffet-app/src/constants.ts
@@ -11,3 +11,8 @@ export const ORDERS_PER_PAGE: number = 5;
 export const CART_LOCAL_STORAGE_KEY: string = "cartItems";
 
 export const FETCH_URL: string = "https://wlczak.vlastas.cc/backend/api";
+
+export const WEBSOCKET_URL = (channel: string): string =>
+  window.location.protocol === "https:"
+    ? FETCH_URL.replace("https", "wss").replace("api", channel)
+    : FETCH_URL.replace("http", "ws").replace("api", channel);
