03f8605d1f93ea604fc574603978ddc5433c444a
Adam Vlček <vlcek-adam@seznam.cz>
2024-12-13 10:22:10 +0100

    order api base class

---
+++ buffet-api/src/Api/OrderApi.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Buffet\Api;
+
+use Buffet\Types\ApiResponse;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface as RequestInterface;
+
+class OrderApi
+{
+
+    /**
+     * @param  RequestInterface    $request
+     * @return ResponseInterface
+     */
+    public function createOrder(ApiResponse $request): ApiResponse
+    {
+
+    }
+}
