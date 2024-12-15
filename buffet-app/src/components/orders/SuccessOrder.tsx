69e7c71ad6775c3f46eadb2b13df517b90663e1e
ptakondrej <ptakondrej@spseplzen.cz>
2024-12-15 15:42:40 +0100

    Edit success order

--- buffet-app/src/components/orders/SuccessOrder.tsx
+++ buffet-app/src/components/orders/SuccessOrder.tsx
@@ -1,35 +1,20 @@
-import { useNavigate } from "react-router-dom";
-import { useFetch } from "../../hooks/useFetch";
-import { useEffect } from "react";
-import Loading from "../Loading";
+import { useLocation } from "react-router-dom";
 import { Order } from "../../types";
-import { FETCH_URL } from "../../constants";
+import PageNotFound from "../error/PageNotFound";
 
 const SuccessOrder = () => {
-  const navigate = useNavigate();
+  const location = useLocation();
+  const order = location.state?.order as Order;
 
-  const { data, error, isLoading } = useFetch<Order>(FETCH_URL, {
-    requestType: "success-order",
-  });
-
-  useEffect(() => {
-    if (!data) {
-      navigate("/");
-    }
-  }, []);
-
-  if (isLoading) {
-    return <Loading />;
-  }
-
-  if (error) {
-    return <div className="text-center text-xl text-white">{error}</div>;
+  if (!order) {
+    return <PageNotFound />;
   }
 
   return (
     <div className="flex flex-col items-center justify-center text-white">
       <h1 className="text-center text-2xl font-bold">
-        Objednávka <span className="text-cyan-500">#{data?.id}</span> byla
+        Objednávka{" "}
+        <span className="text-cyan-500">#{order && order.pickUpId}</span> byla
         úspěšně vytvořena!
       </h1>
       <h2 className="text-xl">Nezapomeňte si ji včas vyzvednout.</h2>
