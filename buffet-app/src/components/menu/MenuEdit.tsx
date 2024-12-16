3a06d71b649aa1e23757913fbd517434781523c0
ptakondrej <ptakondrej@spseplzen.cz>
2024-12-16 22:05:02 +0100

    Rename bar

--- buffet-app/src/components/menu/MenuEdit.tsx
+++ buffet-app/src/components/menu/MenuEdit.tsx
@@ -7,7 +7,7 @@ import Loading from "../Loading";
 import PagingButtons from "../PagingButtons";
 import MenuItemAdd from "./MenuItemAdd";
 import MenuItemEdit from "./MenuItemEdit";
-import MenuEditBar from "./MenuEditBar";
+import MenuItemEditBar from "./MenuItemEditBar";
 
 const MenuEdit = () => {
   const {
@@ -21,7 +21,14 @@ const MenuEdit = () => {
   } = useBackendPaging<MenuItemType>("getMenu", ITEMS_PER_PAGE - 1);
   const [isBarOpen, setIsBarOpen] = useState<boolean>(false);
 
-  const handleBarOpen = () => {
+  const [editItem, setEditItem] = useState<MenuItemType | null>(null);
+
+  const handleBarOpen = (id?: number) => {
+    if (id) {
+      setEditItem(dataList.find((item) => item.id === id) || null);
+    } else {
+      setEditItem(null);
+    }
     setIsBarOpen(!isBarOpen);
   };
 
@@ -36,16 +43,30 @@ const MenuEdit = () => {
   }
   return (
     <>
-      {isBarOpen && <MenuEditBar handleBarOpen={handleBarOpen} />}
       <div className="flex flex-col items-center justify-center gap-5">
         <h1 className="text-3xl font-bold text-white">Úprava menu</h1>
         {/* {isLoading && <Loading size={30} />} */}
+        <div className="flex flex-row-reverse items-start gap-5">
+          {isBarOpen && (
+            <MenuItemEditBar
+              handleBarOpen={handleBarOpen}
+              menuItem={editItem}
+            />
+          )}
 
-        <div className="grid grid-cols-1 gap-10 md:grid-cols-2 xl:grid-cols-4">
-          <MenuItemAdd handleBarOpen={handleBarOpen} />
-          {dataList.map((item) => (
-            <MenuItemEdit key={item.id} item={item} />
-          ))}
+          <div
+            className={`grid grid-cols-1 gap-10 md:grid-cols-2 ${isBarOpen ? "xl:grid-cols-3" : "xl:grid-cols-4"}`}
+          >
+            <MenuItemAdd handleBarOpen={handleBarOpen} />
+            {dataList.map((item) => (
+              <MenuItemEdit
+                key={item.id}
+                item={item}
+                handleBarOpen={handleBarOpen}
+                isBarOpen={isBarOpen}
+              />
+            ))}
+          </div>
         </div>
 
         <PagingButtons
