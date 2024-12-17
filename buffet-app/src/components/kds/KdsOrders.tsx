d78fe8df1843f74eae2022a94803b2a798e167a2
ptakondrej <ptakondrej@spseplzen.cz>
2024-12-17 21:45:45 +0100

    Create kds orders custom hook

--- buffet-app/src/components/kds/KdsOrders.tsx
+++ buffet-app/src/components/kds/KdsOrders.tsx
@@ -1,60 +1,13 @@
 import Loading from "../Loading";
 import KdsStatusBar from "./KdsStatusBar";
-import { WEBSOCKET_URL } from "../../constants";
-import useWebSocket, { ReadyState } from "react-use-websocket";
-import { Order } from "../../types";
 import KdsOrder from "./KdsOrder";
 
 import KdsDeliveryOrder from "./KdsDeliveryOrder";
-import { useEffect, useState } from "react";
-import { usePaging } from "../../hooks/usePaging";
-import { useUser } from "../../hooks/useUser";
+import { useKdsOrders } from "../../hooks/useKdsOrders";
 
 const KdsOrders = () => {
-  const { token } = useUser();
-  const { sendMessage, lastJsonMessage, readyState } = useWebSocket(
-    WEBSOCKET_URL("kds"),
-    {
-      onOpen: () => {
-        if (token) {
-          sendMessage(JSON.stringify({ requestType: "subscribe", token }));
-        }
-      },
-      shouldReconnect: () => true,
-    },
-  );
-
-  console.log(lastJsonMessage ? lastJsonMessage.payload : "Čekám na data");
-
-  const [orders, setOrders] = useState<Order[]>([]);
-
-  useEffect(() => {
-    if (lastJsonMessage) {
-      setOrders(lastJsonMessage.payload.data);
-    }
-  }, [lastJsonMessage]);
-
-  const { dataList: pendingOrders } = usePaging<Order>(
-    orders
-      ?.filter(
-        (order) => order.status === "preparing" || order.status === "sent",
-      )
-      .sort((a, b) => a.pickupDate.localeCompare(b.pickupDate)),
-    8,
-  );
-
-  const { dataList: waitingOrders } = usePaging<Order>(
-    orders?.filter((order) => order.status === "waiting"),
-    8,
-  );
-
-  const handleStatusChange = (updatedOrder: Order) => {
-    setOrders((prevOrders) =>
-      prevOrders.map((order) =>
-        order.id === updatedOrder.id ? updatedOrder : order,
-      ),
-    );
-  };
+  const { pendingOrders, waitingOrders, handleStatusChange, isLoading, error } =
+    useKdsOrders();
 
   return (
     <div className="mx-auto flex w-[85.5%] flex-col justify-center">
@@ -73,10 +26,10 @@ const KdsOrders = () => {
         waiting={waitingOrders?.length}
       />
 
-      {readyState === ReadyState.CONNECTING ? (
+      {isLoading ? (
         <Loading size={30} />
-      ) : readyState === ReadyState.CLOSED ? (
-        <p className="text-center text-4xl text-white">Chyba</p>
+      ) : error ? (
+        <p className="text-center text-4xl text-white">{error}</p>
       ) : (
         <div className="flex flex-row items-start justify-between">
           <div className="flex flex-wrap gap-2">
