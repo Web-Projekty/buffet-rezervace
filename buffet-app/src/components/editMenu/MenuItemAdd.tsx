ffe7bd57ee4bf958c4c1e483f19f1c01ddc54805
ptakondrej <ptakondrej@spseplzen.cz>
2024-12-18 22:12:55 +0100

    Disable button when bar is open

--- buffet-app/src/components/editMenu/MenuItemAdd.tsx
+++ buffet-app/src/components/editMenu/MenuItemAdd.tsx
@@ -4,9 +4,10 @@ import { Plus } from "lucide-react";
 
 type MenuItemAddProps = {
   handleBarOpen: (id?: number) => void;
+  isBarOpen: boolean;
 };
 
-const MenuItemAdd = ({ handleBarOpen }: MenuItemAddProps) => {
+const MenuItemAdd = ({ handleBarOpen, isBarOpen }: MenuItemAddProps) => {
   const handleAdd = () => {
     handleBarOpen();
   };
@@ -14,10 +15,10 @@ const MenuItemAdd = ({ handleBarOpen }: MenuItemAddProps) => {
   return (
     <motion.div
       {...menuItemShowAnimation(0.5)}
-      className="relative flex h-[24rem] w-[16rem] cursor-pointer flex-col items-center justify-center gap-2 rounded-lg bg-slate-900 p-4 text-white shadow-sm shadow-black xl:h-[26rem] xl:w-[18rem]"
-      onClick={handleAdd}
+      className={`relative flex h-[24rem] w-[16rem] ${isBarOpen ? "cursor-not-allowed" : "cursor-pointer"} flex-col items-center justify-center gap-2 rounded-lg bg-slate-900 p-4 text-white shadow-sm shadow-black xl:h-[26rem] xl:w-[18rem]`}
+      onClick={isBarOpen ? undefined : handleAdd}
     >
-      <Plus size={64} onClick={handleAdd} />
+      <Plus size={64} />
     </motion.div>
   );
 };
