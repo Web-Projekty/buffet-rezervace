89a93a5a8d7e8678079aee0fe1c66687009b5232
ptakondrej <ptakondrej@spseplzen.cz>
2024-12-18 18:55:07 +0100

    Add background color

--- buffet-app/tailwind.config.js
+++ buffet-app/tailwind.config.js
@@ -35,6 +35,7 @@ export default {
         transparentBlack: "rgba(0, 0, 0, 0.5)",
         descriptionColor: "#9ca3af",
         interactiveColor: "#06b6d4",
+        backgroundColor: "#1e293b",
       },
     },
   },
