5b12c171320e4409f472adebcfa8d588a5df864f
ptakondrej <ptakondrej@spseplzen.cz>
2024-12-19 21:50:38 +0100

    Add variants and more

--- buffet-app/src/components/cart/CartItem.tsx
+++ buffet-app/src/components/cart/CartItem.tsx
@@ -1,5 +1,8 @@
 import useCart from "../../store/CartStore";
 import { MenuItem } from "../../types";
+import MenuItemButtons from "../menu/MenuItemButtons";
+import MenuItemImage from "../menu/MenuItemImage";
+import MenuItemVariants from "../menu/MenuItemVariants";
 import { formatCurrency } from "../utils/utils";
 
 type CartItemProps = {
@@ -7,39 +10,44 @@ type CartItemProps = {
 };
 
 const CartItem = ({ item }: CartItemProps) => {
-  const { getItemQuantity, removeFromCart, addToCart } = useCart();
+  const { getItemQuantity } = useCart();
 
-  const handleAddItem = (item: MenuItem) => {
-    addToCart(item);
-  };
+  const variantsPrice = item.variants
+    ? item.variants.reduce((acc, variant) => acc + variant.price, 0)
+    : 0;
 
-  const handleRemoveItem = (id: number) => {
-    removeFromCart(id);
-  };
   return (
     <div
       key={item.id}
-      className="flex h-[90px] w-[300px] flex-row items-center justify-between rounded-lg bg-slate-900 p-4 text-white shadow-md"
+      className="grid w-[22rem] grid-cols-1 gap-5 rounded-lg bg-slate-700 p-4 text-white shadow-sm shadow-black md:w-[32rem] md:grid-cols-2"
     >
-      <div className="flex flex-row gap-2">
-        <img
-          src={item.image}
-          alt={item.name}
-          className="h-16 w-16 rounded-md object-cover"
-        />
-        <div className="flex flex-col">
-          <h1 className="text-xl font-bold">{item.name}</h1>
+      <div className="flex h-full flex-col justify-between gap-5">
+        <div className="flex flex-col items-start">
+          <h1 className="text-lg font-bold">{item.name}</h1>
+          <p className="text-base text-descriptionColor">{item.description}</p>
+        </div>
 
-          <p className="font-bold text-gray-200">
-            {formatCurrency(item.price * getItemQuantity(item.id))}
-          </p>
+        <div className="flex flex-col gap-2">
+          <MenuItemVariants menuItemId={item.id} variants={item.variants} />
+          <hr />
+          <div className="flex items-center justify-between text-lg">
+            <div className="flex flex-row gap-3">
+              <p>Celkem</p>
+              <span>x {getItemQuantity(item.id)}</span>
+            </div>
+            <p className="font-bold">
+              {formatCurrency(
+                getItemQuantity(item.id) * (item.price + variantsPrice),
+              )}
+            </p>
+          </div>
+          <div className="flex flex-row items-center justify-center">
+            <MenuItemButtons item={item} />
+          </div>
         </div>
       </div>
-
-      <div className="flex flex-row gap-2">
-        <button onClick={() => handleRemoveItem(item.id)}>-</button>
-        <span>{getItemQuantity(item.id)}</span>
-        <button onClick={() => handleAddItem(item)}>+</button>
+      <div className="hidden md:flex md:h-full md:flex-col md:items-center md:justify-center">
+        <MenuItemImage {...item} />
       </div>
     </div>
   );
