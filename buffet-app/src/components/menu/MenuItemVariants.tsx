842d4c77d5abb271e62faacbf18d47fc5289e96d
ptakondrej <ptakondrej@spseplzen.cz>
2024-12-19 21:55:41 +0100

    Work on variants component

--- buffet-app/src/components/menu/MenuItemVariants.tsx
+++ buffet-app/src/components/menu/MenuItemVariants.tsx
@@ -1,19 +1,37 @@
 import { MenuItem } from "../../types";
+import { formatCurrency } from "../utils/utils";
 
 type MenuItemVariantsProps = {
+  menuItemId: MenuItem["id"];
   variants: MenuItem["variants"];
 };
 
+export const dummyVariants = [
+  {
+    name: "Kečup",
+    price: 5,
+  },
+  {
+    name: "Hořčice",
+    price: 5,
+  },
+];
+
 const MenuItemVariants = ({ variants }: MenuItemVariantsProps) => {
   return (
-    <div>
+    <div className="flex flex-col gap-1">
       {variants &&
         variants.map((item) => {
           return (
-            <div key={item.name} className="flex justify-between gap-2">
-              <p>{item.name}</p>
-              <p>{item.price} Kč</p>
-            </div>
+            <label className="flex items-center gap-2" key={item.name}>
+              <input type="checkbox" />
+              <div className="flex w-full items-center justify-between">
+                <p>{item.name}</p>
+                <p className="text-descriptionColor">
+                  +{formatCurrency(item.price)}
+                </p>
+              </div>
+            </label>
           );
         })}
     </div>
