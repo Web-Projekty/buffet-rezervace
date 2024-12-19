50f70db7db2ffbf7ef6fbc0ad2fda5b974f89989
ptakondrej <ptakondrej@spseplzen.cz>
2024-12-19 21:54:02 +0100

    Update styles

--- buffet-app/src/components/menu/MenuItemText.tsx
+++ buffet-app/src/components/menu/MenuItemText.tsx
@@ -1,19 +1,14 @@
-import { formatCurrency } from "../utils/utils";
-
 type MenuItemTextProps = {
   name: string;
   price: number;
   description: string;
 };
 
-const MenuItemText = ({ name, price, description }: MenuItemTextProps) => {
+const MenuItemText = ({ name, description }: MenuItemTextProps) => {
   return (
     <div className="flex flex-col justify-center gap-1">
-      <div className="flex h-[4rem] items-center justify-between font-bold">
+      <div className="flex h-auto w-full flex-col items-start justify-between font-bold">
         <h1 className="w-full rounded-lg text-xl">{name}</h1>
-        <span className="rounded-lg text-lg font-normal italic">
-          {formatCurrency(price)}
-        </span>
       </div>
       <hr />
       <p className="rounded-lg px-2 text-descriptionColor">{description}</p>
