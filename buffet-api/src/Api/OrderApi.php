dfe4a418d7f32fb54edd3c4f3298b4ae9cd737b6
Adam Vlček <vlcek-adam@seznam.cz>
2024-12-21 21:32:38 +0100

    added TempModel

--- buffet-api/src/Api/OrderApi.php
+++ buffet-api/src/Api/OrderApi.php
@@ -2,7 +2,6 @@
 
 namespace Buffet\Api;
 
-use Buffet\Types\ApiResponse;
 use Buffet\Database\Models\TimeslotModel;
 use Buffet\Types\Exceptions\NegativeValueException;
 use Buffet\Types\Time;
---
+++ buffet-api/src/Database/Models/TempModel.php
@@ -0,0 +1,28 @@
+<?php
+
+declare (strict_types = 1);
+
+namespace Buffet\Database\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TempModel extends Model
+{
+    /**
+     * @var string
+     */
+    protected $table = 'temp';
+
+    /**
+     * @var array<string>
+     */
+    protected $fillable = [
+        'id',
+        'date',
+        'startTime',
+        'endTime',
+        'orderLimit',
+        'orderCount'
+    ];
+
+}
