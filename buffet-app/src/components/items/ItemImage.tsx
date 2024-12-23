319858cb3052fe02be336b6035f5f51aca6069a5
ptakondrej <ptakondrej@spseplzen.cz>
2024-12-23 16:46:25 +0100

    Add lazy loading for image

--- buffet-app/src/components/items/ItemImage.tsx
+++ buffet-app/src/components/items/ItemImage.tsx
@@ -1,7 +1,6 @@
-import { useEffect, useState } from "react";
 import { Allergen } from "../../types";
+import LazyImage from "../LazyImage";
 import ItemAllergens from "./ItemAllergens";
-import { motion } from "framer-motion";
 
 type ItemImageProps = {
   image: string;
@@ -10,38 +9,9 @@ type ItemImageProps = {
 };
 
 const ItemImage = ({ image, name, allergens }: ItemImageProps) => {
-  const [loading, setLoading] = useState(true);
-  const [imageSrc, setImageSrc] = useState<string>(image);
-
-  useEffect(() => {
-    setLoading(true);
-    setImageSrc(`${image}?${new Date().getTime()}`);
-  }, [image]);
-
-  const handleImageLoad = () => {
-    setLoading(false);
-  };
-
-  const handleImageError = () => {
-    setLoading(false);
-  };
-
   return (
     <div className="relative">
-      {loading && (
-        <div className="skeleton h-[12rem] w-[16rem] animate-pulse rounded-lg bg-slate-700"></div>
-      )}
-      <motion.img
-        src={imageSrc}
-        alt={name}
-        className={`h-[12rem] w-[16rem] rounded-lg object-cover ${loading ? "hidden" : "block"}`}
-        onLoad={handleImageLoad}
-        onError={handleImageError}
-        initial={{ opacity: 0 }}
-        animate={{ opacity: loading ? 0 : 1 }}
-        transition={{ duration: 0.5 }}
-      />
-
+      <LazyImage image={image} alt={name} />
       <ItemAllergens allergens={allergens} />
     </div>
   );
