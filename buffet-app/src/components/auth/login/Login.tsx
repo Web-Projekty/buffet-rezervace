afff1431d75ea7046017522fea0c856b5f9532c6
ptakondrej <ptakondrej@spseplzen.cz>
2024-12-23 19:03:13 +0100

    Add React import

--- buffet-app/src/components/auth/login/Login.tsx
+++ buffet-app/src/components/auth/login/Login.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { motion } from "framer-motion";
 import Input from "../../Input";
 import Button from "../../Button";
