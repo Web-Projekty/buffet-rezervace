8890ba041ed9f02321b424950ae6da97c21ef953
ptakondrej <ptakondrej@spseplzen.cz>
2024-12-25 22:00:17 +0100

    Add back lazy load for UserMenu

--- buffet-app/src/components/menu/Menu.tsx
+++ buffet-app/src/components/menu/Menu.tsx
@@ -1,10 +1,9 @@
 import { lazy, Suspense } from "react";
 import { useUser } from "../../hooks/useUser";
 import { Fallback } from "../../main.tsx";
-import UserMenu from "./UserMenu.tsx";
 
 const MenuEdit = lazy(() => import("./editMenu/MenuEdit.tsx"));
-// const UserMenu = lazy(() => import("./UserMenu.tsx"));
+const UserMenu = lazy(() => import("./UserMenu.tsx"));
 
 const Menu = () => {
   const { isAdmin } = useUser();
@@ -14,9 +13,9 @@ const Menu = () => {
       <MenuEdit />
     </Suspense>
   ) : (
-    // <Suspense fallback={<Fallback />}>
-    <UserMenu />
-    // </Suspense>
+    <Suspense fallback={<Fallback />}>
+      <UserMenu />
+    </Suspense>
   );
 };
 
