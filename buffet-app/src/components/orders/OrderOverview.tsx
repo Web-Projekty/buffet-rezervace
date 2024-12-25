60d8afcbfb4a8249be8ce1461f621d9f6e41f20a
ptakondrej <ptakondrej@spseplzen.cz>
2024-12-25 17:14:51 +0100

    Add OrderHistory

--- buffet-app/src/components/orders/OrderOverview.tsx
+++ buffet-app/src/components/orders/OrderOverview.tsx
@@ -1,25 +1,33 @@
+import { useState } from "react";
 import OrderTracking from "./OrderTracking";
+import OrderHistory from "./OrderHistory";
 
-const Buttons = [
-  { id: 1, name: "Přehled" },
-  { id: 2, name: "Historie" },
-];
+const Buttons = [{ name: "Přehled" }, { name: "Historie" }];
+
+type Page = "Přehled" | "Historie";
 
 const OrderOverview = () => {
+  const [page, setPage] = useState<Page>("Přehled");
+
+  const handlePageChange = (page: Page) => {
+    setPage(page);
+  };
+
   return (
-    <div className="m-auto grid h-[40rem] w-[75rem] grid-cols-3 gap-2">
+    <div className="m-auto grid w-[75rem] grid-cols-3 gap-2">
       <div className="col-span-1 flex flex-col gap-2 rounded-lg bg-slate-900 p-2 text-white">
-        {Buttons.map(({ id, name }) => (
+        {Buttons.map(({ name }) => (
           <button
-            key={id}
+            key={name}
             className="rounded-lg border-2 border-white p-2 text-white"
+            onClick={() => handlePageChange(name as Page)}
           >
             {name}
           </button>
         ))}
       </div>
-      <div className="col-span-2 flex flex-col gap-2 rounded-lg bg-slate-900 p-2 text-white">
-        <OrderTracking />
+      <div className="col-span-2 flex flex-col items-center gap-2 rounded-lg bg-slate-900 p-2 text-white">
+        {page === "Přehled" ? <OrderTracking /> : <OrderHistory />}
       </div>
     </div>
   );
