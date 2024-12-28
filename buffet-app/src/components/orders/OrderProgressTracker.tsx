e7adc7c685f1c2a5ce8b17fccd14b48d2736ac96
ptakondrej <ptakondrej@spseplzen.cz>
2024-12-28 11:36:19 +0100

    Add tracking by order status

--- buffet-app/src/components/orders/OrderProgressTracker.tsx
+++ buffet-app/src/components/orders/OrderProgressTracker.tsx
@@ -1,21 +1,31 @@
 type ProgressTrackerProps = {
-  steps: Step[];
   currentStep: number;
 };
 
-export type Step = {
+type Step = {
   label: string;
   icon: string;
-  status: "active" | "completed" | "inactive";
+  step: number;
 };
 
-const ProgressTracker = ({ steps }: ProgressTrackerProps) => {
+const steps: Step[] = [
+  { label: "Odesláno", icon: "📤", step: 0 },
+  { label: "V přípravě", icon: "👨‍🍳", step: 1 },
+  { label: "K vyzvednutí", icon: "🍔", step: 2 },
+  { label: "Vyzvednuto", icon: "✔️", step: 3 },
+];
+
+const ProgressTracker = ({ currentStep }: ProgressTrackerProps) => {
   return (
     <div className="flex w-full items-center justify-center">
-      <div className="flex items-center gap-28">
+      <div className="flex items-center gap-14 md:gap-28">
         {steps.map((step, index) => {
-          const isCompleted = step.status === "completed";
-          const isActive = step.status === "active";
+          const isCompleted: boolean =
+            currentStep < 3
+              ? step.step < currentStep
+              : step.step <= currentStep;
+          const isActive = step.step === currentStep;
+          const isCancelled = currentStep === -1;
 
           return (
             <div
@@ -45,10 +55,14 @@ const ProgressTracker = ({ steps }: ProgressTrackerProps) => {
               </div>
 
               {index < steps.length - 1 && (
-                <div className="absolute left-0 mx-2 h-[2px] w-[12rem] bg-gray-400">
+                <div className="absolute left-0 mx-2 h-[2px] w-[8rem] bg-gray-400 md:w-[12rem]">
                   <div
                     className={`h-full ${
-                      isCompleted ? "bg-blue-500" : "bg-gray-400"
+                      isCompleted
+                        ? "bg-blue-500"
+                        : isCancelled
+                          ? "bg-red-400"
+                          : "bg-gray-400"
                     }`}
                   ></div>
                 </div>
--- buffet-app/src/components/orders/OrderTracking.tsx
+++ buffet-app/src/components/orders/OrderTracking.tsx
@@ -1,12 +1,6 @@
+import { useMemo } from "react";
 import { Order } from "../../types";
-import ProgressTracker, { Step } from "./OrderProgressTracker";
-
-const steps: Step[] = [
-  { label: "Odesláno", icon: "📤", status: "completed" },
-  { label: "V přípravě", icon: "👨‍🍳", status: "active" },
-  { label: "K vyzvednutí", icon: "🍔", status: "inactive" },
-  { label: "Vyzvednuto", icon: "✔️", status: "inactive" },
-];
+import ProgressTracker from "./OrderProgressTracker";
 
 const getTextBySteps = (step: number) => {
   switch (step) {
@@ -18,40 +12,65 @@ const getTextBySteps = (step: number) => {
       return "Vaše objednávka je připravena k vyzvednutí.";
     case 3:
       return "Vaše objednávka byla úspěšně vyzvednuta.";
+    case 4:
+      return "Objednávka byla zrušena vámi či provozovatelem.";
+    case -1:
+      return "Žádná objednávka nebyla dosud vytvořena.";
     default:
       return "Neznámý stav objednávky.";
   }
 };
 
-const OrderTracking = () => {
-  const order: Order | null = null;
-  const currentStep = steps.findIndex((step) => step.status === "active");
+const getCurrentStep = (order: Order | null): number => {
+  if (order === null) return -1;
+  if (order.status === "sent") return 0;
+  if (order.status === "preparing") return 1;
+  if (order.status === "waiting") return 2;
+  if (order.status === "done") return 3;
+  if (order.status === "cancelled" || order.status === "storno") return 4;
+  return -1;
+};
 
-  //   if (!order) {
-  //     return (
-  //       <div className="flex w-full flex-col items-center justify-center gap-16 rounded-lg p-6 text-white">
-  //         <h1>Nemáte aktivní žádnou objednávku.</h1>
-  //       </div>
-  //     );
-  //   }
+const OrderTracking = () => {
+  const order: Order | null = useMemo(
+    () => ({
+      id: 1,
+      userId: 1,
+      status: "preparing",
+      date: Date.now().toLocaleString(),
+      pickupDate:
+        new Date(Date.now() + 1000).toLocaleTimeString() +
+        " - " +
+        new Date(Date.now() + 301000).toLocaleTimeString(),
+      items: [],
+      pickUpId: "#264",
+    }),
+    [],
+  );
+  const currentStep: number = useMemo(
+    () => (order ? getCurrentStep(order) : -2),
+    [order],
+  );
 
   return (
     <div className="flex w-full flex-col items-center justify-center gap-16 rounded-lg p-6 text-white">
       <div className="flex flex-col gap-5">
         <h1 className="text-lg font-semibold">Aktuální objednávka</h1>
-        <ProgressTracker steps={steps} currentStep={currentStep} />
+        <ProgressTracker currentStep={currentStep} />
       </div>
 
-      <p>{getTextBySteps(currentStep)}</p>
+      <p className="text-center">{getTextBySteps(currentStep)}</p>
 
-      <div className="flex flex-row gap-5">
-        <h2>Obsah</h2>
-        <div>
-          <p>Vaše objednávka bude k vyzvednutí pod číslem</p>
-          <h3>#384</h3>
-          <p>(10:15 - 10:20)</p>
+      {order ? (
+        <div className="flex flex-row gap-5">
+          <h2>Obsah</h2>
+          <div>
+            <p>Vaše objednávka bude k vyzvednutí pod číslem</p>
+            <h3>{order.pickUpId}</h3>
+            <p>{order.pickupDate}</p>
+          </div>
         </div>
-      </div>
+      ) : null}
     </div>
   );
 };
