933db4220bb8a80e09d84e002e53efe39ee82fe6
ptakondrej <ptakondrej@spseplzen.cz>
2025-01-05 09:27:23 +0100

    Add paging back

--- buffet-app/src/components/orders/OrderHistory.tsx
+++ buffet-app/src/components/orders/OrderHistory.tsx
@@ -1,29 +1,21 @@
 import Order from "./Order";
 import { usePaging } from "../../hooks/usePaging";
+import { AnimatePresence } from "framer-motion";
+import { ORDERS_PER_PAGE } from "../../constants";
+import Loading from "../ui/Loading";
+import useOrders from "../../hooks/useOrders";
 import { Order as OrderType } from "../../types";
 import PagingButtons from "../ui/PagingButtons";
-import { AnimatePresence } from "framer-motion";
-import { FETCH_URL, ORDERS_PER_PAGE } from "../../constants";
-import { useFetch } from "../../hooks/useFetch";
-import Loading from "../ui/Loading";
-import { useUser } from "../../hooks/useUser";
-
-type OrderData = {
-  data: OrderType[];
-};
 
 const OrderHistory = () => {
-  const { token } = useUser();
-
-  const { data, isLoading, error } = useFetch<OrderData>(
-    FETCH_URL,
-    { requestType: "getOrders", token: token },
-    { data: [] },
-    [token],
-  );
+  const { orders, isLoading, error } = useOrders();
 
   const { currentPage, totalPagesCount, dataList, arrayOfPages, handlePage } =
-    usePaging<OrderType>(data?.data, ORDERS_PER_PAGE, "orderPage");
+    usePaging<OrderType>(
+      orders?.data ? orders.data : [],
+      ORDERS_PER_PAGE,
+      "orderPage",
+    );
 
   if (isLoading) {
     return <Loading size={30} />;
@@ -35,20 +27,24 @@ const OrderHistory = () => {
 
   return (
     <div className="flex w-auto flex-col gap-2">
-      <h1 className="text-2xl">Tvá historie objednávek ({dataList.length})</h1>
-      <AnimatePresence>
-        <ul className="flex flex-col gap-2">
-          {dataList.map((order) => (
-            <Order key={order.id} order={order} />
-          ))}
-        </ul>
-      </AnimatePresence>
-      <PagingButtons
-        currentPage={currentPage}
-        totalPagesCount={totalPagesCount}
-        listOfPages={arrayOfPages}
-        handlePage={handlePage}
-      />
+      <h1 className="text-2xl font-bold">
+        Tvá historie objednávek ({orders?.data.length})
+      </h1>
+      <div className="flex min-h-[25rem] flex-col justify-between gap-2">
+        <AnimatePresence>
+          <ul className="flex flex-col items-center gap-2">
+            {dataList.map((order) => (
+              <Order key={order.pickUpId + "" + order.userId} order={order} />
+            ))}
+          </ul>
+        </AnimatePresence>
+        <PagingButtons
+          currentPage={currentPage}
+          totalPagesCount={totalPagesCount}
+          listOfPages={arrayOfPages}
+          handlePage={handlePage}
+        />
+      </div>
     </div>
   );
 };
