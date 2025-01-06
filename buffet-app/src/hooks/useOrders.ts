0491c549077d823e7289b5bda755d113c7e67a4e
ptakondrej <ptakondrej@spseplzen.cz>
2025-01-06 13:23:23 +0100

    Add option for order count

--- buffet-app/src/hooks/useOrders.ts
+++ buffet-app/src/hooks/useOrders.ts
@@ -1,16 +1,16 @@
 import { useEffect, useState } from "react";
-import { FETCH_URL } from "../constants";
-import { Order } from "../types";
 import { useFetch } from "./useFetch";
 import { useUser } from "./useUser";
+import { Order } from "../types";
+import { FETCH_URL } from "../constants";
 
 type OrderData = {
   data: Order[];
 };
 
-const useOrders = () => {
+const useOrders = (itemsCount: "all" | number, page?: number) => {
   const { token } = useUser();
-  const [orders, setOrders] = useState<OrderData | null>(null);
+  const [orders, setOrders] = useState<Order[] | null>(null);
   const [latestOrder, setLatestOrder] = useState<Order | null>(null);
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
@@ -21,24 +21,32 @@ const useOrders = () => {
     error: fetchError,
   } = useFetch<OrderData>(
     FETCH_URL,
-    { requestType: "getOrders", token: token },
+    {
+      requestType: "getOrders",
+      token: token,
+      page: page ? page : undefined,
+      itemsCount: itemsCount === "all" ? undefined : itemsCount,
+    },
     { data: [] },
     [token],
   );
 
   useEffect(() => {
     if (fetchedOrders && fetchedOrders.data) {
-      if (
-        !orders ||
-        JSON.stringify(orders.data) !== JSON.stringify(fetchedOrders.data)
-      ) {
-        setOrders(fetchedOrders);
-        setLatestOrder(fetchedOrders.data[0]);
+      const sortedOrders = [...fetchedOrders.data].sort((a, b) => {
+        return (
+          new Date(b.dateCreated).getTime() - new Date(a.dateCreated).getTime()
+        );
+      });
+
+      if (!orders || JSON.stringify(orders) !== JSON.stringify(sortedOrders)) {
+        setOrders(sortedOrders);
+        setLatestOrder(sortedOrders[0]);
       }
     }
     setIsLoading(fetchLoading);
     setError(fetchError);
-  }, [fetchedOrders, fetchLoading, fetchError, orders]);
+  }, [fetchedOrders, fetchLoading, fetchError]);
 
   return { orders, latestOrder, error, isLoading };
 };
