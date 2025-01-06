51b471617a6a936fd651de85e64d11a0f8afb51a
ptakondrej <ptakondrej@spseplzen.cz>
2025-01-06 22:06:15 +0100

    Edit layout

--- buffet-app/src/components/orders/Order.tsx
+++ buffet-app/src/components/orders/Order.tsx
@@ -1,37 +1,32 @@
-import { AnimatePresence, motion } from "framer-motion";
+import { motion } from "framer-motion";
 import { Order as OrderType } from "../../types";
-import { scaleUpAnimation } from "../../animations";
 import { ChevronLeft } from "lucide-react";
 import { useOrder } from "../../hooks/useOrder";
 import OrderPrice from "./OrderPrice";
 import OrderItems from "./OrderItems";
 import Button from "../ui/Button";
+import { lazy, Suspense } from "react";
+import { Fallback } from "../../main";
+
+const OrderDetails = lazy(() => import("./OrderDetails"));
 
 type OrderProps = {
   order: OrderType;
 };
 
-type AnimationWrapperProps = {
-  children: React.ReactNode;
-  keyValue: string;
-  className?: string;
-};
-
-const AnimationWrapper = ({
-  children,
-  keyValue,
-  className,
-}: AnimationWrapperProps) => {
-  return (
-    <motion.div key={keyValue} className={className} {...scaleUpAnimation()}>
-      {children}
-    </motion.div>
-  );
-};
-
 const Order = ({ order }: OrderProps) => {
-  const { color, isOpen, toggleOpen, status, statusText, handleStatus } =
-    useOrder(order);
+  const {
+    color,
+    isOpen,
+    toggleOpen,
+    status,
+    statusText,
+    handleStatus,
+    dateCreated,
+    pickUpDate,
+    startTime,
+    endTime,
+  } = useOrder(order);
 
   const handleOpen = () => {
     toggleOpen();
@@ -41,6 +36,11 @@ const Order = ({ order }: OrderProps) => {
     handleStatus("storno");
   };
 
+  const showPickUpDate =
+    status === "storno" || status === "cancelled" || status === "done"
+      ? false
+      : true;
+
   return (
     <motion.div
       initial={{ x: 100, opacity: 0 }}
@@ -48,12 +48,21 @@ const Order = ({ order }: OrderProps) => {
       transition={{ duration: 0.5 }}
       className={`relative flex w-auto flex-col rounded-lg bg-backgroundColor p-4 md:w-[45rem]`}
     >
-      <div className="flex flex-row items-center justify-between text-xl">
+      <div className="flex flex-row items-center justify-between">
         <div
           className={`absolute left-0 h-[64px] w-2 ${color} round-bl-lg rounded-bl-lg rounded-tl-lg`}
         ></div>
-        <div className="flex flex-row items-center gap-2 text-xl">
-          <h2 className="font-bold">#{order.pickUpId}</h2>
+        <div className="flex w-full flex-row items-center gap-16">
+          <h2 className="text-xl font-bold">#{order.pickUpId}</h2>
+
+          <p className="text-base font-semibold">
+            Stav: <span className="font-normal">{statusText}</span>
+          </p>
+          {showPickUpDate && (
+            <p className="text-base">
+              {pickUpDate + " " + startTime + " - " + endTime}
+            </p>
+          )}
         </div>
         <div className="flex flex-row items-center">
           <ChevronLeft
@@ -63,7 +72,6 @@ const Order = ({ order }: OrderProps) => {
           />
         </div>
       </div>
-
       <motion.ul
         initial="closed"
         animate={isOpen ? "open" : "closed"}
@@ -72,31 +80,14 @@ const Order = ({ order }: OrderProps) => {
           closed: { height: 0, opacity: 0 },
         }}
         transition={{ duration: 0.3 }}
-        className="flex flex-col gap-3 overflow-hidden"
       >
-        <OrderItems items={order.items} />
-
-        <hr />
-
-        <div className="flex flex-col justify-between md:flex-row">
-          <OrderPrice items={order.items} />
-          <div className="flex flex-col items-center gap-2 md:flex-row">
-            {status === "sent" && (
-              <Button key="cancel-button" onClick={handleCancel}>
-                Zrušit
-              </Button>
-            )}
-
-            <AnimatePresence>
-              <AnimationWrapper
-                keyValue="status-button"
-                className="flex flex-row items-center gap-2"
-              >
-                <span>{statusText}</span>
-              </AnimationWrapper>
-            </AnimatePresence>
-          </div>
-        </div>
+        <Suspense fallback={<Fallback />}>
+          <OrderDetails
+            isOpen={isOpen}
+            items={order.items}
+            dateCreated={dateCreated}
+          />
+        </Suspense>
       </motion.ul>
     </motion.div>
   );
