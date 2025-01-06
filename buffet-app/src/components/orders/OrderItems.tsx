0cc64fa2067ae5304555883e8c00311f1e6db4d1
ptakondrej <ptakondrej@spseplzen.cz>
2025-01-06 22:05:53 +0100

    Add dummy items

--- buffet-app/src/components/orders/OrderItems.tsx
+++ buffet-app/src/components/orders/OrderItems.tsx
@@ -1,13 +1,36 @@
+import { CartItem } from "../../store/CartStore";
 import { MenuItem } from "../../types";
 import { formatCurrency } from "../utils/utils";
 
 type OrderItemsProps = {
-  items: MenuItem[] | string;
+  items: CartItem[] | string;
 };
 
 const OrderItems = ({ items }: OrderItemsProps) => {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col gap-1">
+      <ul className="flex flex-col gap-2">
+        <li className="flex w-[240px] flex-row items-center justify-center gap-2">
+          <h3>Polévka</h3>
+          <div className="mt-3 flex-1 border-b-2 border-dotted border-white"></div>
+          <p>20 Kč</p>
+        </li>
+        <li className="flex w-[240px] flex-row items-center justify-center gap-2">
+          <h3>Polévka</h3>
+          <div className="mt-3 flex-1 border-b-2 border-dotted border-white"></div>
+          <p>20 Kč</p>
+        </li>
+        <li className="flex w-[240px] flex-row items-center justify-center gap-2">
+          <h3>Polévka</h3>
+          <div className="mt-3 flex-1 border-b-2 border-dotted border-white"></div>
+          <p>20 Kč</p>
+        </li>
+      </ul>
+      <hr className="w-[240px]" />
+      <div className="flex w-[240px] flex-row items-center justify-between gap-2">
+        <h3>Celkem</h3>
+        <p>60 Kč</p>
+      </div>
       {/* {items.map((item) => (
         <li
           key={item.id}
