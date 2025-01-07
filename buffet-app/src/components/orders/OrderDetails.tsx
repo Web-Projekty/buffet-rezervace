7855ae1db1d70fb427cb08a4adbaff640982e9ed
ptakondrej <ptakondrej@spseplzen.cz>
2025-01-07 22:19:32 +0100

    Add cancel button

--- buffet-app/src/components/orders/OrderDetails.tsx
+++ buffet-app/src/components/orders/OrderDetails.tsx
@@ -1,33 +1,43 @@
-import { lazy } from "react";
 import { CartItem } from "../../store/CartStore";
-
-const OrderItems = lazy(() => import("./OrderItems"));
+import { OrderStatus } from "../../types";
+import OrderButtons from "./OrderButtons";
+import OrderItems from "./OrderItems";
 
 type OrderDetailsProps = {
   items: CartItem[];
   dateCreated: string;
+  status: OrderStatus;
+  orderId: number;
 };
 
-const OrderDetails = ({ items, dateCreated }: OrderDetailsProps) => {
+const OrderDetails = ({
+  items,
+  dateCreated,
+  status,
+  orderId,
+}: OrderDetailsProps) => {
   return (
     <div
       className={`mt-5 grid grid-cols-2 justify-between overflow-hidden px-4`}
     >
       <div>
         <p className="font-semibold">Objednané položky:</p>
-
         <OrderItems items={items} />
       </div>
 
-      <div className="">
-        <p>
-          <span className="font-semibold">Vytvořeno:</span> {dateCreated}
-        </p>
-        <div>
+      <div className="flex flex-col justify-between gap-2">
+        <div className="flex flex-col">
           <p>
-            <span className="font-semibold">Platba:</span>
+            <span className="font-semibold">Vytvořeno:</span> {dateCreated}
           </p>
+          <div>
+            <p>
+              <span className="font-semibold">Platba:</span>
+            </p>
+          </div>
         </div>
+
+        <OrderButtons status={status} orderId={orderId} />
       </div>
     </div>
   );
