b34e7a4063326db1c4635ab3c61331888c24b6a5
Adam Vlček <vlcek-adam@seznam.cz>
2025-01-09 18:40:28 +0100

    passwordConfirm

--- buffet-api/src/Api/BuffetApi.php
+++ buffet-api/src/Api/BuffetApi.php
@@ -171,7 +171,7 @@ class BuffetApi
 
     function handleRegister(ApiResponse $response): ApiResponse
     {
-        $response->setRequestKeys(["username", "password", "fullName", "email", "tel"]);
+        $response->setRequestKeys(["username", "password", "passwordConfirm", "fullName", "email", "tel"]);
         $response->setPayloadKeys(["msg"]);
 
         $auth = new AuthApi;
