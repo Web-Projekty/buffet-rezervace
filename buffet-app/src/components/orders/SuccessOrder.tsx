e0a7ea800301f1c35fd502afe996cc3aeb925bef
ptakondrej <ptakondrej@spseplzen.cz>
2025-01-11 07:44:07 +0100

    Change displayed order id

--- buffet-app/src/components/orders/SuccessOrder.tsx
+++ buffet-app/src/components/orders/SuccessOrder.tsx
@@ -6,15 +6,15 @@ const SuccessOrder = () => {
   const location = useLocation();
   const order = location.state?.order as Order;
 
-  if (!order) {
+  /*if (!order) {
     return <PageNotFound />;
-  }
+  }*/
 
   return (
     <div className="flex flex-col items-center justify-center text-white">
       <h1 className="text-center text-2xl font-bold">
         Objednávka{" "}
-        <span className="text-cyan-500">#{order && order.pickUpId}</span> byla
+        <span className="text-cyan-500">#{order ? order.id : 256}</span> byla
         úspěšně vytvořena!
       </h1>
       <h2 className="text-xl">Nezapomeňte si ji včas vyzvednout.</h2>
