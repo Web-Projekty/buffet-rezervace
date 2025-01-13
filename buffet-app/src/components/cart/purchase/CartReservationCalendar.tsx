f71bfa28f6cd66e0dd94d3311f7324a78f56c9e5
ptakondrej <ptakondrej@spseplzen.cz>
2025-01-13 12:45:14 +0100

    Make loading screen bigger

--- buffet-app/src/components/cart/purchase/CartReservationCalendar.tsx
+++ buffet-app/src/components/cart/purchase/CartReservationCalendar.tsx
@@ -70,63 +70,64 @@ const CartReservationCalendar = ({
     }
   }, [selectedDate, selectedHour, selectedMinute, onTimeSelect]);
 
-  if (loading) {
-    return <Loading />;
-  }
-
   if (error) {
     return <div>{error}</div>;
   }
 
   return (
-    <div className="flex flex-col gap-5 rounded-lg bg-backgroundColor p-6 shadow-md">
-      {/* Day */}
-      <div className="w-full overflow-x-auto">
-        <HorizontalPaging className="m-0 w-[25rem] p-0 md:w-[75rem] 2xl:w-[100rem]">
-          {days.map((day) => (
-            <button
-              key={day.date}
-              className={`flex-shrink-0 rounded px-4 py-2 text-white ${day.available ? (selectedDate === day ? "bg-green-500" : "bg-sky-400") : "bg-gray-600"}`}
-              onClick={() => handleDateClick(day)}
-              disabled={!day.available}
-            >
-              {day.date}
-            </button>
-          ))}
-        </HorizontalPaging>
-      </div>
+    <div className="flex min-h-[25rem] flex-col gap-5 rounded-lg bg-backgroundColor p-6 shadow-md">
+      {!loading ? (
+        <>
+          <div className="w-full overflow-x-auto">
+            <HorizontalPaging className="m-0 w-[25rem] p-0 md:w-[75rem] 2xl:w-[100rem]">
+              {days.map((day) => (
+                <button
+                  key={day.date}
+                  className={`flex-shrink-0 rounded px-4 py-2 text-white ${day.available ? (selectedDate === day ? "bg-green-500" : "bg-sky-400") : "bg-gray-600"}`}
+                  onClick={() => handleDateClick(day)}
+                  disabled={!day.available}
+                >
+                  {day.date}
+                </button>
+              ))}
+            </HorizontalPaging>
+          </div>
 
-      {/* Hours */}
-      <div className="flex flex-col items-start justify-center gap-10 md:flex-row md:gap-6">
-        <div className="grid w-full grid-cols-2 gap-4">
-          {selectedDate?.hours?.map((hour) => (
-            <button
-              key={hour.label}
-              className={`rounded px-4 py-2 text-white disabled:text-white ${hour.available ? (selectedHour === hour ? "bg-green-500" : "bg-sky-400") : "bg-gray-600"}`}
-              onClick={() => hour.available && handleHourClick(hour)}
-              disabled={!hour.available}
-            >
-              {hour.label}
-            </button>
-          ))}
-        </div>
+          <div className="flex flex-col items-start justify-center gap-10 md:flex-row md:gap-6">
+            <div className="grid w-full grid-cols-2 gap-4">
+              {selectedDate?.hours?.map((hour) => (
+                <button
+                  key={hour.label}
+                  className={`rounded px-4 py-2 text-white disabled:text-white ${hour.available ? (selectedHour === hour ? "bg-green-500" : "bg-sky-400") : "bg-gray-600"}`}
+                  onClick={() => hour.available && handleHourClick(hour)}
+                  disabled={!hour.available}
+                >
+                  {hour.label}
+                </button>
+              ))}
+            </div>
 
-        {/* Minutes */}
-        <div className="grid w-full grid-cols-2 gap-4">
-          {selectedHour?.minutes?.map((minute) => {
-            return (
-              <button
-                key={minute.label}
-                className={`text-nowrap rounded px-4 py-2 text-center text-white disabled:text-white ${minute.available ? (selectedMinute === minute ? "bg-green-500" : "bg-sky-400") : "bg-gray-600"}`}
-                onClick={() => minute.available && handleMinuteClick(minute)}
-                disabled={!minute.available}
-              >
-                {minute.label}
-              </button>
-            );
-          })}
-        </div>
-      </div>
+            <div className="grid w-full grid-cols-2 gap-4">
+              {selectedHour?.minutes?.map((minute) => {
+                return (
+                  <button
+                    key={minute.label}
+                    className={`text-nowrap rounded px-4 py-2 text-center text-white disabled:text-white ${minute.available ? (selectedMinute === minute ? "bg-green-500" : "bg-sky-400") : "bg-gray-600"}`}
+                    onClick={() =>
+                      minute.available && handleMinuteClick(minute)
+                    }
+                    disabled={!minute.available}
+                  >
+                    {minute.label}
+                  </button>
+                );
+              })}
+            </div>
+          </div>
+        </>
+      ) : (
+        <Loading size={30} />
+      )}
     </div>
   );
 };
