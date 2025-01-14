18f6ed9e9e86248967b55bdd3257d7b1b9195daf
ptakondrej <ptakondrej@spseplzen.cz>
2025-01-14 22:07:48 +0100

    Add array to map items

--- buffet-app/src/components/orders/OrderItems.tsx
+++ buffet-app/src/components/orders/OrderItems.tsx
@@ -23,23 +23,15 @@ const OrderItems = ({ items }: OrderItemsProps) => {
   return (
     <div className="flex flex-col gap-1">
       <ul className="flex flex-col gap-2">
-        <OrderItem item={items[0]} />
+        {items.map((item) => (
+          <OrderItem key={item.id} item={item} />
+        ))}
       </ul>
       <hr className="w-full md:w-[240px]" />
       <div className="flex w-full flex-row items-center justify-between gap-2 md:w-[240px]">
         <h3>Celkem</h3>
         <p>60 Kč</p>
       </div>
-      {/* {items.map((item) => (
-        <li
-          key={item.id}
-          className="flex w-[240px] flex-row items-center justify-center gap-2"
-        >
-          <h3>{item.name}</h3>
-          <div className="mt-3 flex-1 border-b-2 border-dotted border-white"></div>
-          <p>{formatCurrency(item.price)}</p>
-        </li>
-      ))} */}
     </div>
   );
 };
