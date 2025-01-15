384312b6fc58f347114a542dc4a163cac47172bc
ptakondrej <ptakondrej@spseplzen.cz>
2025-01-15 22:28:05 +0100

    Optimize component

--- buffet-app/src/components/kds/KdsOrders.tsx
+++ buffet-app/src/components/kds/KdsOrders.tsx
@@ -9,6 +9,20 @@ const KdsOrders = () => {
   const { pendingOrders, waitingOrders, handleStatusChange, isLoading, error } =
     useKdsOrders();
 
+  const renderPendingOrders = () => {
+    return pendingOrders && pendingOrders.length > 0 ? (
+      pendingOrders.map((order) => (
+        <KdsOrder
+          key={order.id}
+          order={order}
+          onStatusChange={handleStatusChange}
+        />
+      ))
+    ) : (
+      <p className="text-4xl text-white">Žádné objevnávky nejsou dostupné</p>
+    );
+  };
+
   return (
     <div className="mx-auto flex w-[85.5%] flex-col justify-center">
       <KdsStatusBar
@@ -32,21 +46,7 @@ const KdsOrders = () => {
         <p className="text-center text-4xl text-white">{error}</p>
       ) : (
         <div className="flex flex-row items-start justify-between">
-          <div className="flex flex-wrap gap-2">
-            {pendingOrders && pendingOrders.length > 0 ? (
-              pendingOrders.map((order) => (
-                <KdsOrder
-                  key={order.id}
-                  order={order}
-                  onStatusChange={handleStatusChange}
-                />
-              ))
-            ) : (
-              <p className="text-4xl text-white">
-                Žádné objevnávky nejsou dostupné
-              </p>
-            )}
-          </div>
+          <div className="flex flex-wrap gap-2">{renderPendingOrders()}</div>
           <div className="flex flex-col gap-2">
             {waitingOrders && waitingOrders.length > 0
               ? waitingOrders.map((order) => (
