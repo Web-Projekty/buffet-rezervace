e48b0561e851b5e15759fe12690ba1327c0ffe0b
ptakondrej <ptakondrej@spseplzen.cz>
2025-01-16 21:59:55 +0100

    Edit styles

--- buffet-app/src/components/kds/KdsOrder.tsx
+++ buffet-app/src/components/kds/KdsOrder.tsx
@@ -1,6 +1,7 @@
 import { Order, OrderItem, OrderStatus } from "../../types";
 import Button from "../ui/Button";
 import { useOrder } from "../../hooks/useOrder";
+import OrderItems from "../orders/OrderItems";
 
 type KdsOrderProps = {
   order: Order;
@@ -9,7 +10,8 @@ type KdsOrderProps = {
 };
 
 const KdsOrder = ({ order, onStatusChange, items }: KdsOrderProps) => {
-  const { color, status, mappedItems } = useOrder(order, true, items);
+  const { color, status, mappedItems, pickUpDate, startTime, endTime } =
+    useOrder(order, true, items);
 
   const handleDoneOrder = () => {
     onStatusChange(order.id, "waiting");
@@ -42,20 +44,21 @@ const KdsOrder = ({ order, onStatusChange, items }: KdsOrderProps) => {
   };
 
   return (
-    <div className="flex h-auto w-full flex-col bg-white sm:w-[15rem] md:w-[302px]">
-      <div className={`w-full ${color} px-4 py-3 text-xl font-bold`}>
-        {order && `#${order.pickUpId}`}
+    <div className="flex h-auto w-full flex-col justify-between bg-white sm:w-[15rem] md:w-[302px]">
+      <div className="flex w-full flex-col gap-2">
+        <div
+          className={`flex w-full flex-row items-center justify-between ${color} px-4 py-3 text-xl font-bold`}
+        >
+          <p>{order && `#${order.pickUpId}`}</p>
+          <p className="text-lg font-normal">
+            {order && pickUpDate + " " + startTime + "-" + endTime}
+          </p>
+        </div>
+        <div className="w-full px-4 py-2">
+          <OrderItems mappedItems={mappedItems} />
+        </div>
       </div>
-      <div className="w-full px-4 py-2">
-        <ul className="m-2 flex-grow">
-          <div className="flex flex-row gap-1 text-base font-normal">
-            {mappedItems.map((item) => (
-              <p key={item.id}>{item.name}</p>
-            ))}
-          </div>
-        </ul>
-        {renderButtons()}
-      </div>
+      <div className="px-4 py-2">{renderButtons()}</div>
     </div>
   );
 };
