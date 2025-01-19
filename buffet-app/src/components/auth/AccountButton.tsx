27a5a0b2c60aeae56b2148092cd6b980ae355e08
ptakondrej <ptakondrej@spseplzen.cz>
2025-01-19 21:21:25 +0100

    Add account button variant

--- buffet-app/src/components/auth/AccountButton.tsx
+++ buffet-app/src/components/auth/AccountButton.tsx
@@ -1,12 +1,30 @@
 import { User } from "lucide-react";
 import { Link, useLocation } from "react-router-dom";
 
-const AccountButton = () => {
+type AccountButtonProps = {
+  type?: "navbar" | "menu";
+  onClick?: () => void;
+};
+
+const AccountButton = ({ type = "navbar", onClick }: AccountButtonProps) => {
   const location = useLocation();
   const isOnDashboard: boolean =
     location.pathname === "/account" ||
     location.pathname === "/login" ||
     location.pathname === "/register";
+
+  if (type === "menu") {
+    return (
+      <Link
+        to="/account"
+        className="flex h-16 w-full items-center justify-center gap-2 bg-primary text-white"
+        onClick={onClick}
+      >
+        <User size={24} />
+        <span>Účet</span>
+      </Link>
+    );
+  }
 
   return (
     <Link
