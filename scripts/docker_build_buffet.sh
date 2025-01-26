f0353796c120870581a41b68fdc8919b3eebd7b3
Adam Vlček <vlcek-adam@seznam.cz>
2025-01-26 15:41:24 +0100

    without interactive flag

--- scripts/docker_build_buffet.sh
+++ scripts/docker_build_buffet.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 docker builder prune -f
-docker run --rm -it -v $(pwd)/../buffet-app:/app -w /app node:latest sh -c "echo 'Installing npm packges:' && npm install && npm run build"
-docker run --rm -it -v $(pwd)/../buffet-api:/app -w /app composer:lts sh -c "echo 'Installing compooser packges:' && composer install"
+docker run --rm -t -v $(pwd)/../buffet-app:/app -w /app node:latest sh -c "echo 'Installing npm packges:' && npm install && npm run build"
+docker run --rm -t -v $(pwd)/../buffet-api:/app -w /app composer:lts sh -c "echo 'Installing compooser packges:' && composer install"
 cd ..
 rm -rf buffet-api/dist
 cp -r buffet-app/dist buffet-api
