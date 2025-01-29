fed83cd835423fc93f4544871885a5cf3f3ba043
Adam Vlček <vlcek-adam@seznam.cz>
2025-01-29 15:05:17 +0100

    updated run scripts

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 clear
-docker exec -it php-apache /var/www/html/vendor/bin/phpunit tests --testdox --colors=always --coverage-text --coverage-html tests/result --display-deprecations --display-phpunit-deprecations --fail-on-deprecation --fail-on-phpunit-deprecation
+docker exec -it php-apache_dev /var/www/html/vendor/bin/phpunit tests --testdox --colors=always --coverage-text --coverage-html tests/result --display-deprecations --display-phpunit-deprecations --fail-on-deprecation --fail-on-phpunit-deprecation
 #docker exec -it php-apache /var/www/html/vendor/bin/phpunit tests --coverage-text
 
 # export XDEBUG_MODE=debug
