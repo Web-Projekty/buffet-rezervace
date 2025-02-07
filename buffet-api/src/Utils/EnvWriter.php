eb11d98799b684fe69c928eb1ad2bd625e0f6b18
Adam Vlček <vlcek-adam@seznam.cz>
2025-02-07 17:01:22 +0100

    added functional EnvWrite
    side note: this code has been mainly written by ai and could be faulty, but who has time to check...

--- buffet-api/src/Utils/EnvWriter.php
+++ buffet-api/src/Utils/EnvWriter.php
@@ -1,11 +1,40 @@
 <?php
 
-declare(strict_types=1);
+declare (strict_types = 1);
 
 namespace Buffet\Utils;
 
+use Buffet\Types\Settings;
+
+require __DIR__ . '/../../vendor/autoload.php';
+
 class EnvWriter
 {
-    
-}
+    private static string $envPath = __DIR__ . "/../../conf/.env";
 
+    /**
+     * @param Settings $key
+     * @param string   $value
+     */
+    public static function write(Settings $key, string $value): void
+    {
+        if (!file_exists(self::$envPath)) {
+            EnvReader::createEnv();
+        }
+        if (!$envContent = file_get_contents(self::$envPath)) {
+            return;
+        }
+
+        if (preg_match('/^' . preg_quote((string) $key->value, '/') . '=/m', $envContent)) {
+            $envContent = preg_replace(
+                '/^' . preg_quote((string) $key->value, '/') . '=.*/m',
+                (string) $key->value . "=$value",
+                $envContent
+            );
+        } else {
+            $envContent .= PHP_EOL . (string) $key->value . "=$value";
+        }
+
+        file_put_contents(self::$envPath, $envContent);
+    }
+}
