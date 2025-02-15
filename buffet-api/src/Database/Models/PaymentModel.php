13349a90ac04dd629dd9d382e07e56c47acd533c
Adam Vlček <vlcek-adam@seznam.cz>
2025-02-15 20:39:48 +0100

    fixed KDS notification

--- buffet-api/src/Database/Models/PaymentModel.php
+++ buffet-api/src/Database/Models/PaymentModel.php
@@ -58,18 +58,25 @@ class PaymentModel extends Model
     }
 
     /**
-     * @param int $paymentId
+     * @param int $thePayId
      */
-    public static function setPaid(int $paymentId): void
+    public static function setPaid(int $thePayId): void
     {
-        $paymentQuery = PaymentModel::query()->where('thePayId', $paymentId);
-        $orderId = $paymentQuery->first()["id"];
+        $paymentQuery = PaymentModel::query()->where('thePayId', $thePayId);
+
+        ob_start();
+        #var_dump($paymentId);
+        $paymentId = $paymentQuery->first()->toArray();
+
+        $order = OrderModel::query()->where("paymentId", "=", $paymentId)->get()->toArray();
+        
+        error_log(ob_get_clean());
+        #error_log($orderId);
         if ($paymentQuery->get()->count() === 0) {
             throw new \Exception("Payment not found", 1);
         }
         $paymentQuery->update(['paid' => 1]);
 
-        $order = OrderModel::query()->where('id', "=", $orderId)->get()->toArray();
 
         WebsocketClient::send("kds", json_encode(["requestType" => "publish", "token" => JWTApi::getAdminToken(), "eventType" => EventTypes::CreateOrder, "payload" => $order]));
     }
