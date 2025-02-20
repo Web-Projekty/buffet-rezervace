a786db4d6aebc5e218a1d9e304781ae01dff19bc
ptakondrej <ptakondrej@spseplzen.cz>
2025-02-20 19:35:52 +0100

    Create toast messages

---
+++ buffet-app/src/components/utils/toastMessages.ts
@@ -0,0 +1,45 @@
+export const toastMessages = {
+  login: {
+    success: "Přihlášení proběhlo úspěšně",
+    error: "Chyba při přihlášení",
+  },
+  register: {
+    success: "Registrace proběhla úspěšně",
+    error: "Chyba při registraci",
+  },
+  createMenuItem: {
+    success: "Položka menu byla vytvořena",
+    error: "Chyba při vytváření položky menu",
+  },
+  updateMenuItem: {
+    success: "Položka menu byla aktualizována",
+    error: "Chyba při aktualizaci položky menu",
+  },
+  deleteMenuItem: {
+    success: "Položka menu byla smazána",
+    error: "Chyba při mazání položky menu",
+  },
+  updateOrder: {
+    success: "Objednávka byla aktualizována",
+    error: "Chyba při aktualizaci objednávky",
+  },
+  passwordChange: {
+    success: "Heslo bylo změněno",
+    error: "Chyba při změně hesla",
+  },
+  profileChange: {
+    success: "Profil byl změněn",
+    error: "Chyba při změně profilu",
+  },
+  order: {
+    success: "Objednávka byla odeslána",
+    error: "Chyba při odesílání objednávky",
+  },
+  cart: {
+    full: "Košík je plný",
+    fullItem: "Maximální počet této položky",
+    empty: "Košík je prázdný",
+    add: "Položka byla přidána do košíku",
+    remove: "Položka byla odebrána z košíku",
+  },
+};
