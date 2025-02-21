45b4d7f87288464e6b2abd67796c9420c7ee6c34
ptakondrej <ptakondrej@spseplzen.cz>
2025-02-21 11:19:10 +0100

    Add category click scroll

--- buffet-app/src/components/menu/MenuSections.tsx
+++ buffet-app/src/components/menu/MenuSections.tsx
@@ -1,34 +1,27 @@
-import { Category, MenuItem } from "../../types";
 import { motion } from "framer-motion";
+import { MutableRefObject } from "react";
 import { menuItemShowAnimation } from "../../animations";
+import { Category, MenuItem } from "../../types";
 import MenuItems from "./MenuItems";
 
 type MenuSectionsProps = {
-  filteredCategories: Category[];
-  menuItems: MenuItem[] | null;
   categories: Category[];
+  menuItems: MenuItem[] | null;
+  refs: MutableRefObject<{ [key: number]: HTMLElement | null }>;
 };
 
-const MenuSections = ({
-  filteredCategories,
-  menuItems,
-  categories,
-}: MenuSectionsProps) => {
-  if (filteredCategories.length <= 0) {
-    return (
-      <p className="italic text-white">
-        "Meow? (Waiting for something to happen?)"
-      </p>
-    );
-  }
-
+const MenuSections = ({ categories, menuItems, refs }: MenuSectionsProps) => {
   return (
     <motion.section
       {...menuItemShowAnimation(0.5)}
       className="flex flex-col items-start"
     >
-      {filteredCategories.map((category) => (
-        <section className="mt-5 flex flex-col gap-3" key={category.id}>
+      {categories.map((category) => (
+        <section
+          className="mt-5 flex flex-col gap-3"
+          key={category.id}
+          ref={(el) => (refs.current[category.id] = el)}
+        >
           <div className="flex flex-col items-center gap-2 md:flex-row md:justify-between md:gap-0">
             <h1 className="text-4xl font-bold text-white">{category.name}</h1>
             <p className="text-white md:mr-5">{category.description}</p>
