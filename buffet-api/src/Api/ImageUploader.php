7f0e2a4169ea84918199dbfdcab1532e43cb3cc6
Adam Vlček <vlcek-adam@seznam.cz>
2025-02-22 15:58:02 +0100

    image uploader progress

--- buffet-api/src/Api/ImageUploader.php
+++ buffet-api/src/Api/ImageUploader.php
@@ -6,11 +6,12 @@ namespace Buffet\Api;
 
 use Buffet\Types\ApiResponse;
 use Psr\Http\Message\ServerRequestInterface;
-use Psr\Http\Message\UploadedFileInterface;
 use Slim\Psr7\UploadedFile;
 
 class ImageUploader
 {
+
+    public string $mime;
     /**
      * @param ServerRequestInterface $request
      * @param int                    $imageId
@@ -31,43 +32,97 @@ class ImageUploader
         $uploadedFile = $uploadedFiles['image'];
 
         if ($uploadedFile->getError() === UPLOAD_ERR_OK) {
-            $filename = $this->moveUploadedFile($fileDirectory, $uploadedFile, $imageId);
+            $filename = $this->moveUploadedFile($baseDirectory, $fileDirectory, $uploadedFile, $imageId);
         }
     }
 
     /**
-     * @param string                $directory
-     * @param UploadedFileInterface $uploadedFile
+     * @param string       $directory
+     * @param UploadedFile $uploadedFile
      */
-    function moveUploadedFile(string $directory, UploadedFileInterface $uploadedFile, int $imageId): string
+    function moveUploadedFile(string $baseDirectory, string $directory, UploadedFile $uploadedFile, int $imageId): string
     {
         $extension = pathinfo($uploadedFile->getClientFilename(), PATHINFO_EXTENSION);
 
-        $filename = strval($imageId) . '.' . $extension;
-        $fullPath = $directory . DIRECTORY_SEPARATOR . $filename;
-
-        if (file_exists($fullPath)) {
-            unlink($fullPath);
+        if (!file_exists($baseDirectory . DIRECTORY_SEPARATOR . "tmp")) {
+            mkdir($baseDirectory . DIRECTORY_SEPARATOR . "tmp");
         }
 
-        $uploadedFile->moveTo($fullPath);
+        $filename = strval($imageId) . '.png';
+        $tempFilename = $tempFileName = 'temp_' . time() . '_' . bin2hex(random_bytes(4)) . '.' . $extension;
+
+        $tempPath = $baseDirectory . DIRECTORY_SEPARATOR . "tmp" . DIRECTORY_SEPARATOR . $tempFileName;
+        $targetPath = $directory . DIRECTORY_SEPARATOR . $filename;
+
+        if ($this->isImage($uploadedFile)) {
+            var_dump($this->mime);
+        }
+
+        $mime = $this->mime;
+
+        $uploadedFile->moveTo($tempPath);
+
+        if (file_exists($tempPath)) {
+            var_dump("exists");
+        }
+        var_dump($mime);
+        switch ($mime) {
+            case 'image/jpeg':
+                $image = imagecreatefromjpeg($tempPath);
+                break;
+            case 'image/png':
+                var_dump("png");
+                $image = imagecreatefrompng($tempPath);
+                break;
+            case 'image/gif':
+                $image = imagecreatefromgif($tempPath);
+                break;
+            case 'image/webp':
+                $image = imagecreatefromwebp($tempPath);
+                break;
+            default:
+                var_dump("false");
+                $image = false;
+        }
+        if ($image) {
+            var_dump($targetPath);
+            var_dump($image);
+            $targetFile = fopen($targetPath, "w+");
+            imagepng($image, $targetFile, 1);
+            var_dump("webp");
+        }
+
+        if (file_exists($targetPath)) {
+            unlink($targetPath);
+        }
 
         return $filename;
     }
 
-/**
- * @param UploadedFile $uploadedFile
- */
+    /**
+     * @param UploadedFile $uploadedFile
+     */
     function isImage(UploadedFile $uploadedFile): bool
     {
-        $finfo = finfo_open(FILEINFO_MIME);
-        $mime = finfo_file($finfo, $uploadedFile->getFilePath());
-        finfo_close($finfo);
+        $mime = mime_content_type($uploadedFile->getFilePath());
+        $this->mime = $mime;
 
         $allowedMimeTypes = ['image/jpeg', 'image/png', 'image/gif', 'image/webp'];
         if (!in_array($mime, $allowedMimeTypes)) {
             return false;
         }
+
         return true;
+    }
+
+/**
+ * @param UploadedFile $uploadedFile
+ */
+    function getMimeType(UploadedFile $uploadedFile): string
+    {
+        $finfo = finfo_open(FILEINFO_MIME);
+        $mime = finfo_file($finfo, $uploadedFile->getFilePath());
+        finfo_close($finfo);
+        return $mime;
     }
 }
