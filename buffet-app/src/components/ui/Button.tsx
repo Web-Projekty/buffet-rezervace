7f0db5c789b9e9eacf140fc68baa505b2b56d33e
ptakondrej <ptakondrej@spseplzen.cz>
2025-03-02 15:49:18 +0100

    Disable if loading

--- buffet-app/src/components/ui/Button.tsx
+++ buffet-app/src/components/ui/Button.tsx
@@ -18,12 +18,12 @@ const Button = ({
   return (
     <button
       className={twMerge(
-        `rounded-md border border-cyan-900 bg-interactiveColor p-2 text-white hover:bg-interactiveHoverColor ${disabled ? "cursor-not-allowed bg-cyan-600" : ""}`,
+        `rounded-md border border-cyan-900 bg-interactiveColor p-2 text-white hover:bg-interactiveHoverColor ${disabled || loading ? "cursor-not-allowed bg-cyan-600 hover:bg-cyan-600" : ""}`,
         className,
       )}
       type={type}
       onClick={onClick}
-      disabled={disabled}
+      disabled={disabled || loading}
     >
       {loading ? <Loading size={20} /> : children}
     </button>
