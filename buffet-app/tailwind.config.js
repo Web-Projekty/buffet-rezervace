38cafe04f46e1b1e70724727e89891f018331bb6
ptakondrej <ptakondrej@spseplzen.cz>
2025-03-03 12:41:31 +0100

    Remove redundant fonts

--- buffet-app/tailwind.config.js
+++ buffet-app/tailwind.config.js
@@ -5,8 +5,6 @@ export default {
     extend: {
       fontFamily: {
         FiraSans: ["Fira Sans", "sans-serif"],
-        SourGummy: ["Sour Gummy", "IBM Plex Mono", "Lato", "sans-serif"],
-        SansSerif: ["sans-serif", "IBM Plex Mono", "Lato"],
       },
       animation: {
         fadeIn: "fadeIn 0.5s ease-in-out",
