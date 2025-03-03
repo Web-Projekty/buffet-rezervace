79039dc8b88a59196bb350e6b6757fccf20096a6
ptakondrej <ptakondrej@spseplzen.cz>
2025-03-03 18:05:34 +0100

    Add className prop

--- buffet-app/src/components/ui/Loading.tsx
+++ buffet-app/src/components/ui/Loading.tsx
@@ -1,14 +1,16 @@
 import { bouncy } from "ldrs";
+import { twMerge } from "tailwind-merge";
 
 bouncy.register();
 
 type LoadingProps = {
   size?: number;
+  className?: string;
 };
 
-const Loading = ({ size = 30 }: LoadingProps) => {
+const Loading = ({ size = 30, className }: LoadingProps) => {
   return (
-    <div className="flex items-center justify-center">
+    <div className={twMerge("flex items-center justify-center", className)}>
       <l-bouncy size={size} speed="1.75" color="white" />
     </div>
   );
