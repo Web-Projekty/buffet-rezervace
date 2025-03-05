37a0721ce29c40fa1953b0e1fba72baec438ce53
ptakondrej <ptakondrej@spseplzen.cz>
2025-03-05 22:09:41 +0100

    Add custom ids

--- buffet-app/src/components/ui/CustomToasts.tsx
+++ buffet-app/src/components/ui/CustomToasts.tsx
@@ -4,14 +4,13 @@ import { CartItemToast, CartItemToastProps } from "./CartRemoveToast";
 const defaultOptions: ToastOptions = {
   duration: 5000,
   className: "group",
-  id: "cart-remove-toast",
 };
 
 export const showCartItemToast = ({ item, addToCart }: CartItemToastProps) => {
   if (!item) return;
 
-  return toast(
-    () => <CartItemToast item={item} addToCart={addToCart} />,
-    defaultOptions,
-  );
+  return toast(() => <CartItemToast item={item} addToCart={addToCart} />, {
+    ...defaultOptions,
+    id: String(item.id),
+  });
 };
