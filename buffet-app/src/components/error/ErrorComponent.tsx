cd3484d9f081a5a0a11ca9127cc11ec35297aaa3
ptakondrej <ptakondrej@spseplzen.cz>
2025-03-05 22:10:11 +0100

    Add tailwind merge

--- buffet-app/src/components/error/ErrorComponent.tsx
+++ buffet-app/src/components/error/ErrorComponent.tsx
@@ -3,6 +3,7 @@ import Button from "../ui/Button";
 import Logo from "../../assets/images/logo.svg";
 import { motion } from "framer-motion";
 import { fadeInAnimation } from "../../animations";
+import { twMerge } from "tailwind-merge";
 
 type Error = {
   title: string;
@@ -25,7 +26,10 @@ const ErrorComponent = ({
 
   return (
     <div
-      className={`flex flex-col items-center justify-center gap-5 overflow-x-hidden bg-slate-800 font-sans text-white ${className}`}
+      className={twMerge(
+        "flex flex-col items-center justify-center gap-5 overflow-x-hidden bg-slate-800 font-sans text-white",
+        className,
+      )}
     >
       <motion.img
         {...fadeInAnimation(0.5)}
