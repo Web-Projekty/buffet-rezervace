7522a78b3d1a44cc9c7e60841f4ab418eeb57ad5
ptakondrej <ptakondrej@spseplzen.cz>
2025-03-05 10:22:29 +0100

    Fix alignment

--- buffet-app/src/components/menu/MenuCategories.tsx
+++ buffet-app/src/components/menu/MenuCategories.tsx
@@ -9,7 +9,9 @@ type MenuCategoriesProps = {
 
 const MenuCategories = ({ categories, onFilter }: MenuCategoriesProps) => {
   return (
-    <HorizontalPaging className="w-[25rem] transition-all duration-1000 ease-in-out sm:w-[30rem] md:w-[35rem] lg:w-[40rem] xl:w-[48rem] 2xl:w-[51rem]">
+    <HorizontalPaging
+      className={`h-[4rem] w-[25rem] transition-all duration-1000 ease-in-out sm:w-[30rem] md:w-[35rem] lg:w-[40rem] xl:w-[48rem] 2xl:w-[51rem] ${categories.length < 4 ? "justify-center" : "justify-start"}`}
+    >
       {categories.map((category) => (
         <MenuCategory
           key={category.id}
