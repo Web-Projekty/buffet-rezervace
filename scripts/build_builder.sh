cdbf047539f442152d845d5fb984c77f4e9f5f33
Adam Vlček <vlcek-adam@seznam.cz>
2025-03-08 17:57:57 +0100

    added predownloaded php image to the builder image

--- scripts/build_builder.sh
+++ scripts/build_builder.sh
@@ -1,2 +1,6 @@
+docker buildx build --platform linux/amd64,linux/arm64 -t php:8.3-apache-bookworm --output type=tar,dest=builder-image/php-image.tar pre-pull
+
 docker build -t wlczak/buffet-builder:latest builder-image
 docker push wlczak/buffet-builder # can replace wlczak with your dockerhub username
+
+rm php-image.tar
