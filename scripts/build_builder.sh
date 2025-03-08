01a0914eb8fd5c4b47aedceebe6570cd7427f173
Adam Vlček <vlcek-adam@seznam.cz>
2025-03-08 20:17:51 +0100

    img load

--- scripts/build_builder.sh
+++ scripts/build_builder.sh
@@ -3,4 +3,4 @@ docker buildx build --platform linux/amd64,linux/arm64 -t php:8.3-apache-bookwor
 docker build -t wlczak/buffet-builder:latest builder-image
 docker push wlczak/buffet-builder # can replace wlczak with your dockerhub username
 
-rm builder-image/php-image.tar
+#rm builder-image/php-image.tar
