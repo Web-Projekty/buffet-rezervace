019d269ae26757091921a2a53a593b5022efe918
Adam Vlček <vlcek-adam@seznam.cz>
2025-03-23 17:05:42 +0100

    added variant filtering

--- buffet-api/src/Api/BuffetApi.php
+++ buffet-api/src/Api/BuffetApi.php
@@ -346,7 +346,7 @@ class BuffetApi
 
             $array[$i]["variants"] = [];
             if (!$variants->where("itemId", "=", $id)->isEmpty()) {
-                $array[$i]["variants"] = array_merge($array[$i]["variants"], $variants->where("itemId", "=", $id)->toArray());
+                $array[$i]["variants"] = array_merge($array[$i]["variants"], $variants->where("itemId", "=", $id)->where("removed", "=", 0)->toArray());
             }
             // parse allergens
             $alergenList = [];
