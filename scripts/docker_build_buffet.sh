cda0a978b32380b091c84d7a50bbd2f2d7af0954
Adam Vlček <vlcek-adam@seznam.cz>
2025-03-29 02:15:53 +0100

    Update docker_build_buffet.sh

--- scripts/docker_build_buffet.sh
+++ scripts/docker_build_buffet.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker builder prune -f
+# docker builder prune -f
 cd ..
 cd buffet-app
 echo 'Installing npm packges:' && npm install && npm run build
@@ -19,7 +19,7 @@ cp -r buffet-app/dist buffet-api
 echo ${DOCKER_TOKEN} | docker login --username wlczak --password-stdin
 docker buildx create --use --name mybuilder # Create builder
 docker buildx inspect mybuilder --bootstrap # Test builder output
-docker buildx build --platform linux/amd64 -t wlczak/buffet --push .
+docker buildx build --platform linux/amd64,linux/arm64/v8 -t wlczak/buffet --push .
 
 ### Old docker builder
 #docker push wlczak/buffet # can replace wlczak with your dockerhub username
