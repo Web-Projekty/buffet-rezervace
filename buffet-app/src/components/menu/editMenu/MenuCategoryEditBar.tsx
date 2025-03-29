07a5388aa68edabc20bedc7ca620036d36665000
ptakondrej <ptakondrej@spseplzen.cz>
2025-03-29 07:57:37 +0100

    Add remove button and prevent close if error

--- buffet-app/src/components/menu/editMenu/MenuCategoryEditBar.tsx
+++ buffet-app/src/components/menu/editMenu/MenuCategoryEditBar.tsx
@@ -5,7 +5,12 @@ import MenuItemEditInput from "./MenuItemEditInput";
 import { onImageChange } from "../../utils/utils";
 import { motion } from "framer-motion";
 import { slideInAnimation } from "../../../animations";
-import { createCategory, updateCategory, uploadImage } from "../../utils/api";
+import {
+  createCategory,
+  removeCategory,
+  updateCategory,
+  uploadImage,
+} from "../../utils/api";
 import { useUser } from "../../../hooks/useUser";
 import ImageInput from "../../ui/ImageInput";
 import toast from "react-hot-toast";
@@ -32,6 +37,8 @@ const MenuCategoryEditBar = ({
   const [itemDescription, setItemDescription] = useState<
     Category["description"]
   >(category?.description || "");
+
+  const [confirmDelete, setConfirmDelete] = useState<boolean>(false);
 
   const handleSave = async () => {
     handleClose();
@@ -76,8 +83,26 @@ const MenuCategoryEditBar = ({
     refetch();
   };
 
+  const handleRemove = async () => {
+    if (!category) return;
+
+    const { error } = await removeCategory(token, category.id);
+
+    if (error) {
+      toast.error("Kategorii se nepodařilo smazat");
+      return;
+    }
+    handleClose();
+    toast.success("Kategorie byla úspěšně smazána");
+    refetch();
+  };
+
   const handleClose = () => {
     handleBarOpen();
+  };
+
+  const handleConfirmDelete = () => {
+    setConfirmDelete(true);
   };
 
   const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -128,6 +153,14 @@ const MenuCategoryEditBar = ({
           >
             Zrušit
           </Button>
+          {category !== null ? (
+            <Button
+              className="m-auto border-red-400 bg-red-400 hover:bg-red-500"
+              onClick={confirmDelete ? handleRemove : handleConfirmDelete}
+            >
+              {confirmDelete ? "Opravdu smazat?" : "Smazat"}
+            </Button>
+          ) : null}
           <Button
             className="m-auto"
             onClick={handleSave}
