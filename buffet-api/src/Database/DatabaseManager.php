89ce9e34f4c9d6ecb97d956f5e442f512641a66b
Adam Vlček <vlcek-adam@seznam.cz>
2025-04-01 18:46:35 +0200

    only sqlite

--- buffet-api/src/Database/DatabaseManager.php
+++ buffet-api/src/Database/DatabaseManager.php
@@ -41,27 +41,6 @@ class DatabaseManager
                 'prefix' => '',
                 'foreign_key_constraints' => false
             ]);
-            
-
-            // Make the Capsule instance available globally via static methods
-            $this->capsule->setAsGlobal();
-
-            // Setup the Eloquent ORM
-            $this->capsule->bootEloquent();
-        }
-        $creds = $this->credentialsManager->getCredentials();
-        if ($creds['success'] == true) {
-            // Eloquent ORM Capsule setup
-            $this->capsule->addConnection([
-                'driver' => 'mysql',
-                'host' => $creds['db_host'] . ":" . $creds["db_port"],
-                'database' => $creds['db_name'],
-                'username' => $creds['db_user'],
-                'password' => $creds['db_pass'],
-                'charset' => 'utf8mb4',
-                'collation' => 'utf8mb4_czech_ci',
-                'prefix' => ''
-            ]);
 
             // Make the Capsule instance available globally via static methods
             $this->capsule->setAsGlobal();
@@ -69,7 +48,28 @@ class DatabaseManager
             // Setup the Eloquent ORM
             $this->capsule->bootEloquent();
         } else {
-            $this->response->setError(Error::FailedDecrypt);
+            $creds = $this->credentialsManager->getCredentials();
+            if ($creds['success'] == true) {
+                // Eloquent ORM Capsule setup
+                $this->capsule->addConnection([
+                    'driver' => 'mysql',
+                    'host' => $creds['db_host'] . ":" . $creds["db_port"],
+                    'database' => $creds['db_name'],
+                    'username' => $creds['db_user'],
+                    'password' => $creds['db_pass'],
+                    'charset' => 'utf8mb4',
+                    'collation' => 'utf8mb4_czech_ci',
+                    'prefix' => ''
+                ]);
+
+                // Make the Capsule instance available globally via static methods
+                $this->capsule->setAsGlobal();
+
+                // Setup the Eloquent ORM
+                $this->capsule->bootEloquent();
+            } else {
+                $this->response->setError(Error::FailedDecrypt);
+            }
         }
     }
 }
