e61d876ecf6193c39d99d688cd94b9311c23d381
ptakondrej <ptakondrej@spseplzen.cz>
2025-04-04 18:35:49 +0200

    Add type for login response

--- buffet-app/src/hooks/useLogin.ts
+++ buffet-app/src/hooks/useLogin.ts
@@ -27,6 +27,18 @@ export type UserData = {
   class: string;
 };
 
+type LoginResponse = {
+  status: string;
+  payload: {
+    token: string;
+    username: string;
+    fullName: string;
+    email: string;
+    isAdmin: boolean;
+    class: string;
+  };
+};
+
 export const useLogin = (loginData: LoginData): UseLoginReturn => {
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<string>("");
@@ -39,10 +51,10 @@ export const useLogin = (loginData: LoginData): UseLoginReturn => {
   const login = async (url?: string) => {
     try {
       setLoading(true);
-      const { data } = await axios.post(FETCH_URL, loginData);
+      const { data } = await axios.post<LoginResponse>(FETCH_URL, loginData);
 
       const success: boolean = data.status === "success";
-      const isAdmin = data.payload.isAdmin === 1;
+      const isAdmin = data.payload.isAdmin;
 
       if (success) {
         signIn({
